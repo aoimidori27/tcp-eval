dfc430f66ad3861f2fc156e78e6ddb71e6914776
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2006-06-20 15:02:17 +0000

    changes paths in makeinitrd

--- trunk/build/makeinitrd.sh
+++ trunk/build/makeinitrd.sh
@@ -1,47 +1,25 @@
 #!/bin/bash
 
-[ "$RC_GOT_FUNCTIONS" != "yes" ] && source /usr/local/bin/functions.sh
-
-FILE_DIR=/opt/mcg-mesh/boot/src/initrd
-INITRD_SRC=/opt/mcg-mesh/boot/bin/initrd/blank.ext3
-INITRD_DST=/opt/mcg-mesh/boot/bin/initrd/initrd
+FILE_DIR=/opt/meshnode/boot/initrd
+INITRD_SRC=/opt/mcg-mesh/boot/initrd/blank.ext3
+INITRD_DST=/opt/mcg-mesh/boot/initrd/initrd
 INITRD_MP=/mnt/initrd
 
-copy_initrd() {
-	ebegin Copy $INITRD_SRC to $INITRD_DST
-	cp $INITRD_SRC $INITRD_DST
-	eend $?
-}
+# Copy initrd
+cp $INITRD_SRC $INITRD_DST
 
-mount_initrd() {
-        ebegin Mount $INITRD_DST '=>' $INITRD_MP
-        if [ ! -d $INITRD_MP ]; then mkdir $INITRD_MP; fi
-	mount $INITRD_DST $INITRD_MP -o loop
-	eend $?
-}
+# Mount initrd
+if [ ! -d $INITRD_MP ]; then mkdir $INITRD_MP; fi
+mount $INITRD_DST $INITRD_MP -o loop
 
-copy_files() {
-        ebegin Copy $FILE_DIR'/*' into $INITRD_MP
-        cp -r $FILE_DIR/* $INITRD_MP
-	eend $?
-}
+# Copy files
+cp -r $FILE_DIR/* $INITRD_MP
 
-umount_initrd() {
-        ebegin Umount $INITRD_MP
-	umount $INITRD_MP
-        eend $?
-}
+# Unmount initrd
+umount $INITRD_MP
 
-gzip_initrd() {
-        ebegin GZip $INITRD_DST
-	cat $INITRD_DST | gzip > $INITRD_DST.gz && \
-        eend $?
-}
+# Gzip initrd
+cat $INITRD_DST | gzip > $INITRD_DST.gz && \
 
-clean_up() {
-        ebegin Delete $INITRD_DST
-        rm $INITRD_DST
-        eend $?
-}
-
-copy_initrd && mount_initrd && copy_files && umount_initrd && gzip_initrd && clean_up
+# Clean up
+rm $INITRD_DST
