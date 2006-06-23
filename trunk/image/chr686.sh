ea8766dbf51900f8e3419415b8fc30a1ca32458c
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2006-06-23 07:33:04 +0000

    ch686.sh gefixt

--- trunk/image/chr686.sh
+++ trunk/image/chr686.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=${0#./chr}
+DIR=`basename $0 .sh | tr -d [:alpha:]`
 mount -o bind /opt/mcg-mesh/images/gentoo/portage /opt/mcg-mesh/images/gentoo/$DIR/usr/portage/
 chroot /opt/mcg-mesh/images/gentoo/$DIR /bin/bash
 umount /opt/mcg-mesh/images/gentoo/$DIR/usr/portage/
