722c647f7315a24c3136e7b071bd05894ffe1dbf
Ulrich Meis <meis@nets.rwth-aachen.de>
2006-06-28 16:04:01 +0000

    little change to 2db_boot

--- trunk/monitor/2db_boot.py
+++ trunk/monitor/2db_boot.py
@@ -2,12 +2,12 @@
 import pyPgSQL, os, socket
 from pyPgSQL import PgSQL
 
-uptime 	= os.popen('cut -f1 -d" " /proc/uptime').read()
-node_id = os.popen('echo -n ${HOSTNAME/mrouter/}').read()
-
 dbcon = PgSQL.connect()
 dbcon.autocommit = True
 cursor = dbcon.cursor()
-cursor.execute("INSERT INTO node.boot (node_id,uptime) VALUES (node.id_by_hostname('%s'),interval '1 second'*(%s::int))" % (socket.gethostname(),uptime))
+cursor.execute(
+	"""INSERT INTO node.boot (node_id,uptime) VALUES 
+		(node.id_by_hostname('%s'),interval '1 second'*%s)
+		""" % (socket.gethostname(),open("/proc/uptime").readline().split(".")[0]))
 cursor.close()
 dbcon.close()
