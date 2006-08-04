5d74929bff66a5a0194481eca4226c376b36d89d
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2006-08-04 10:48:13 +0000

    removed obsolete chroot scripts

--- trunk/image/chr686.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR=`basename $0 .sh | tr -d [:alpha:]`
-mount -o bind /opt/mcg-mesh/images/gentoo/portage /opt/mcg-mesh/images/gentoo/$DIR/usr/portage/
-chroot /opt/mcg-mesh/images/gentoo/$DIR /bin/bash
-umount /opt/mcg-mesh/images/gentoo/$DIR/usr/portage/
