a01b45727c632b19e19a08681672ed89cf3738d3
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2006-08-18 14:41:15 +0000

    mm_madwifi.py optparser fertig

--- trunk/init/mm_madwifi.py
+++ trunk/init/mm_madwifi.py
@@ -5,7 +5,7 @@
 #
 import os, logging, logging.handlers, sre, socket, sys, optparse, time
 from logging import info, debug, warn, error
-#from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE
 
 
 #
@@ -84,19 +84,15 @@ from logging import info, debug, warn, error
 #        iwconfig $WIFIDEV txpower $TXPOWER &>/dev/null
 #        eend $?
 #
-#
-#
-#	echo "Usage: $SCRIPTNAME loadmod | unloadmod | createdev [-w hwdev] [-m mode] | killdev [-d dev] |"
-#	echo "$INDENTATION ifup [-d dev] [-a addr] | setessid [-d dev] [-e essid] | setchannel [-d dev] [-c channel] |"
-#	echo "$INDENTATION settxpower [-d dev] [-x txpower] | autocreate"
 
 
 # main function
 def main():
 	# options parser
-	usage = "usage: %prog [options] COMMAND \n\n" \
-		    "COMMAND:= { loadmod | unloadmod | createdev | killdev | ifup |\n" \
-		    "            ifdown | setessid | setchannel | settxpower }"    
+	usage = "usage: %prog [options] COMMAND \n" \
+		    "where  COMMAND := { loadmod | unloadmod | createdev | killdev |" \
+								"ifup |\n" \
+		    "                    ifdown | setessid | setchannel | settxpower }"    
 	parser = optparse.OptionParser(usage)
 	parser.set_defaults(addr = '169.254.9.1', channel = '1', dev = 'ath0',
 						essid = 'mcg-mesh', mode = 'adhoc', hwdev = 'wifi0',
@@ -134,70 +130,40 @@ def main():
 	# correct numbers of arguments?
 	if len(args) != 1:
 		parser.error("incorrect number of arguments")
+   	
+	# does the command exists?
+	if not args[0] in ('loadmod', 'unloadmod', 'createdev', 'killdev' \
+					   'ifup', 'ifdown', 'setessid', 'setchannel', \
+					   'settxpower'):
+		parser.error('unkown COMMAND %s' %(args[0]))
 
 	# being verbose?
 	if options.verbose:
 		log_level = logging.INFO
 	else:
 		log_level = logging.WARN
-#
-#	# using syslog
-#	if options.syslog:
-#		syslog_facility = logging.handlers.SysLogHandler.LOG_DAEMON
-#		syslog_host = ('logserver', 514)
-#		syslog_format = parser.get_prog_name() + ' %(levelname)s: %(message)s'
-#		syslog_Handler = logging.handlers.SysLogHandler(address = syslog_host,
-#														facility = syslog_facility)
-#		syslog_Handler.setFormatter(logging.Formatter(syslog_format))
-#		logging.getLogger('').addHandler(syslog_Handler)
-#		logging.getLogger('').setLevel(log_level)
-#
-#	# using standard logger
-#	else:
-#		log_format = '%(asctime)s %(levelname)s: %(message)s'
-#		log_datefmt = '%b %d %H:%M:%S'
-#		logging.basicConfig(level = log_level, format = log_format,
-#							datefmt = log_datefmt)
-#
-#	# create database object
-#	info('Create database object...')
-#	database = Database()
-#
-#	# create interface list object and send the results to the database
-#	info('Create interface list object...')
-#	ifaces = InterfaceList.get_ifconfig()
-#	database.send(ifaces, InterfaceList(), InterfaceList())
-#
-#	info('Entering endless loop...')
-#	try:
-#		while True:
-#			# get new ifconfig's output and compare it with the old one
-#			new_ifaces = InterfaceList.get_ifconfig()
-#			(removed_ifaces, changed_ifaces) = ifaces.compare(new_ifaces)
-#			(added_ifaces, changed_ifaces) = new_ifaces.compare(ifaces)
-#
-#			# something change?
-#			if not added_ifaces.empty() or not removed_ifaces.empty() \
-#			   or not changed_ifaces.empty():
-#
-#				# send changes to database
-#				if not added_ifaces.empty():
-#					info('Ifconfig changed. Added ifaces: %s' %(added_ifaces))
-#				if not removed_ifaces.empty():
-#					info('Ifcondig changed. Removed ifaces: %s' %(removed_ifaces))
-#				if not changed_ifaces.empty():
-#					info('Ifconfig changed. Changed ifaces: %s' %(changed_ifaces))
-#				database.send(added_ifaces, removed_ifaces, changed_ifaces)
-#
-#			# update interface list
-#			ifaces = new_ifaces
-#
-#			# sleep for a while
-#			time.sleep(options.time)
-#
-#	except KeyboardInterrupt:
-#		info('Aborted...')
-#
+
+	# using syslog
+	if options.syslog:
+		syslog_facility = logging.handlers.SysLogHandler.LOG_DAEMON
+		syslog_host = ('logserver', 514)
+		syslog_format = parser.get_prog_name() + ' %(levelname)s: %(message)s'
+		syslog_Handler = logging.handlers.SysLogHandler(address = syslog_host,
+														facility = syslog_facility)
+		syslog_Handler.setFormatter(logging.Formatter(syslog_format))
+		logging.getLogger('').addHandler(syslog_Handler)
+		logging.getLogger('').setLevel(log_level)
+
+	# using standard logger
+	else:
+		log_format = '%(asctime)s %(levelname)s: %(message)s'
+		log_datefmt = '%b %d %H:%M:%S'
+		logging.basicConfig(level = log_level, format = log_format,
+							datefmt = log_datefmt)
+
+	# call appropriate function
+	eval
+
 
 
 if __name__ == '__main__':
