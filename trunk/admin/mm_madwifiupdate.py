bc9a80a0cc2e856687a6f5988ec7dab6e904352d
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2006-09-13 13:07:28 +0000

    added a script to update local copy of madwifi-ng sources

---
+++ trunk/admin/mm_madwifiupdate.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+# python imports
+from logging import info, debug, warn, error
+
+# mcg-mesh imports
+from mm_application import Application
+from mm_util import *
+
+
+class MadwifiUpdate(Application):
+	"Class to handle update madwifi files in repos"
+
+	def __init__(self):
+		"Constructor of the object"
+
+		# call the super constructor
+		Application.__init__(self)
+
+	
+		# initialization of the option parser
+		self.parser.set_defaults(verbose = True, syslog=False,
+								 debug = False)
+		
+		# execute object
+		self.main()
+
+
+	def set_option(self):
+		"Set options"
+		
+		# call the super set_option method
+		Application.set_option(self)
+
+		if len(self.args) > 0:
+			self.command = self.args[0]
+			for arg in self.args[1:]:
+				self.command = self.command + arg
+
+
+
+	def madwifiupdate(self):
+		# create temporary directory
+		tmp = "/tmp/madwifiupdate"
+		cmd = "mkdir -p %s" %(tmp)
+		call(cmd, shell = True)
+
+		remote_repos   = madwifiinfos["remote_repos"]
+		remote_module  = madwifiinfos["remote_module"]
+		local_repos    = svninfos["svnrepos"]
+		local_upstream = madwifiinfos["local_upstream"]
+		local_trunk    = madwifiinfos["local_trunk"]
+
+
+		dst = tmp+"/upstream"
+		# check out upstream files
+		info("Checking out madwifi local upstream...")
+		cmd = ("svn","co","-q",
+			   local_repos+local_upstream,
+			   dst)
+		call(cmd, shell = False)
+
+		# get revision
+		cmd = "svn info %s | grep Revision | awk '{print $2;}'" %(dst)	
+		(stdout, stderr) = execute(cmd, shell = True)
+		local_revision = stdout.splitlines()[0]
+		info("Revision of checkout is: %s" %(local_revision))
+
+		# clean up upstream
+		info("Cleaning up upstream checkouts...")
+		cmd = "find %s ! -path '*.svn*' -type f | xargs rm -f" \
+			  %(dst)
+		call(cmd, shell = True)
+
+		# check out trunk from remote repos
+		info("Checking out madwifi trunk from remote...")
+		save_path = os.getcwd()
+		os.chdir(tmp)
+		cmd = ("svn","export","--force","-q",
+			   remote_repos+remote_module,dst)
+		call(cmd, shell = False)
+		os.chdir(save_path)
+	
+		info("Searching updated files...")	
+		# add new files 
+		cmd = "svn st %s | grep '?' | awk '{ print $2; }'" \
+			  %(dst)
+		(stdout,stderr) = execute(cmd, shell = True)
+		debug("Stdout: %s" %stdout)
+		if (stdout != ""):
+			info("Found new files. Adding them...")						
+			cmd = "svn add %s" %(stdout)
+			call(cmd, shell = True)
+
+		# remove files, which were removed in upstream
+		cmd = "svn st %s | grep '!' | awk '{ print $2; }'" \
+			  %(tmp+"/upstream")
+		(stdout,stderr) = execute(cmd, shell = True)
+		debug("Stdout: %s" %stdout)
+		if (stdout != ""):
+			info("Found removed files. Deleting them...")						
+			cmd = "svn rm %s" %(stdout)
+			call(cmd, shell = True)
+
+
+		# commit changes
+		info("Commiting changes...")
+ 		cmd = ("svn","ci",dst,
+ 			   "-m","new madwifi version")
+ 		call(cmd, shell = False)
+
+
+		# switch upstream to trunk
+		info("Switching local checkout to trunk...")
+		cmd = ("svn", "switch", "%s/%s" %(local_repos,
+										  local_trunk),
+			   dst)
+		call(cmd, shell = False)
+		# just for completeness rename it
+		cmd = "mv %s %s/trunk" %(dst,tmp)
+		call(cmd, shell = True)
+		
+
+		# merging changes from upstream local trunk
+		info("Merging changes with local trunk...")
+		cmd = ("svn","merge","-r","%s:HEAD" %(local_revision),
+			   "%s/%s" %(local_repos, local_upstream),
+			   tmp+"/trunk")
+		call(cmd, shell = False)
+
+		# commiting changes to local trunk
+		info("Commiting these changes to repository...")
+		cmd = ("svn","commit", tmp+"/trunk",
+			   "-m","new madwifi version")
+		call(cmd, shell = False)	 
+
+        # cleanup
+		info("Cleaning up %s..." %(tmp))
+		cmd = "rm -rf %s" %(tmp)
+		call(cmd, shell = True)
+
+		info("Done.")
+		
+
+
+	def main(self):
+		"Main method of the MadwifiUpdate object"
+
+		# parse options
+		self.parse_option()
+		
+		# set options
+		self.set_option()
+		
+		# call the corresponding method
+		self.madwifiupdate()	
+				  
+
+
+if __name__ == "__main__":
+	MadwifiUpdate()
