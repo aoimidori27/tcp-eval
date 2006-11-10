150f75b24194ee5f723c10177ff1855bb1a58ee1
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2006-11-10 13:50:19 +0000

    removed different image support from mm_svn.py

--- admin/mm_svn.py
+++ admin/mm_svn.py
@@ -69,64 +69,58 @@ class Subversion(Application):
 	def update(self):
 		# allow group mates to write and exec files
 		os.umask(0007)
-		for image in imageinfos.iterkeys():
-			for node in nodeinfos.iterkeys():
-				svnmappings = copy.copy(svninfos['svnmappings'])
-				# join with node specific mappings
-				svnmappings.update(svninfos['svnmappings_'+node])
+		for node in nodeinfos.iterkeys():
+			svnmappings = copy.copy(svninfos['svnmappings'])
+			# join with node specific mappings
+			svnmappings.update(svninfos['svnmappings_'+node])
 
-				# iterate through svn mappings
-				for src,dst in svnmappings.iteritems():	
-					dst= imageprefix +"/"+ image +"/"+ node + svnprefix + "/" + node + dst
-					src= svninfos["svnrepos"] + src
-					if not os.path.exists(dst):
-						warn("%s got lost! doing a new checkout" % dst)
-						call("mkdir -p %s" % dst, shell = True)
-						cmd = ('svn','checkout',src,dst)
-					else:
-						cmd = ('svn','update',dst)
-					info(cmd)
-					prog = call(cmd,shell=False)
-					
-				# update links
-				if self.updatelinks:
-					# scripts in this folders are copied to /usr/local/bin
-					folders = ("/opt/meshnode/scripts/init",
-							   "/opt/meshnode/scripts/monitor")
-					info("Updating symlinks in /usr/local/bin...")
-					dst = imageprefix + "/" + image + "/" +node +\
-						  "/usr/local/bin"
+			# iterate through svn mappings
+			for src,dst in svnmappings.iteritems():	
+				dst= imageprefix +"/"+ node + svnprefix + "/" + node + dst
+				src= svninfos["svnrepos"] + src
+				if not os.path.exists(dst):
+					warn("%s got lost! doing a new checkout" % dst)
+					call("mkdir -p %s" % dst, shell = True)
+					cmd = ('svn','checkout',src,dst)
+				else:
+					cmd = ('svn','update',dst)
+				info(cmd)
+				prog = call(cmd,shell=False)
 				
-					# just remove every link with mm_ in it
-					cmd = "rm -v `find %s -type l -name 'mm_*'`" %(dst)
-					try:
-						call(cmd,shell=True)
-					except CommandFailed:
-						warn("Removing of links in %s failed" %(dst))
-
-					# recreate those links
-					for d in folders:
-						src = imageprefix + "/" + image + "/" +node + d
-						for f in dircache.listdir(src):
-							if re.match("mm_",f):
-								cmd = "ln -vsf %s/%s %s/%s" %(d,f,dst,f)
-								# use os.system() here because call()
-								# is too slow
-								os.system(cmd) 
-
+			# update links
+			if self.updatelinks:
+				# scripts in this folders are copied to /usr/local/bin
+				folders = ("/opt/"+node+"/scripts/init",
+						   "/opt/"+node+"/scripts/monitor")
+				info("Updating symlinks in /usr/local/bin...")
+				dst = imageprefix + "/" +node +\
+					  "/usr/local/bin"
 			
-
+				# just remove every link with mm_ in it
+				cmd = "rm -v `find %s -type l -name 'mm_*'`" %(dst)
+				try:
+					call(cmd,shell=True)
+				except CommandFailed:
+					warn("Removing of links in %s failed" %(dst))
+					# recreate those links
+				for d in folders:
+					src = imageprefix + "/" +node + d
+					for f in dircache.listdir(src):
+						if re.match("mm_",f):
+							cmd = "ln -vsf %s/%s %s/%s" %(d,f,dst,f)
+							# use os.system() here because call()
+							# is too slow
+							os.system(cmd) 
+			
 
 	# svn status
 	def status(self):
-		for image in imageinfos.iterkeys():
-			for node in nodeinfos.iterkeys():
-				for src,dst in svninfos['svnmappings'].iteritems():	
-					dst= imageprefix +"/"+ image +"/"+ node + svnprefix + dst
-					cmd=('svn','status',dst)
-					info(cmd)
-					prog = call(cmd,shell=False)
-
+		for node in nodeinfos.iterkeys():
+			for src,dst in svninfos['svnmappings'].iteritems():	
+				dst= imageprefix +"/"+ node + svnprefix + "/"+node+ dst
+				cmd=('svn','status',dst)
+				info(cmd)
+				prog = call(cmd,shell=False)
 	
 if __name__ == '__main__':
 	Subversion()
