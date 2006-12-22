394ec24b841719ab0232cf29c5db5d3ae656b6d9
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2006-12-22 16:40:36 +0000

    Bug in openldap-backup.sh

--- server-admin/openldap-backup.sh
+++ server-admin/openldap-backup.sh
@@ -4,4 +4,4 @@ BACKUP_DIR=${1:-/backup/openldap}
 BACKUP_FILE="ldap_dump.ldif"
 
 if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
-slapcat -l $BACKUP_FILE | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
+slapcat | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
