4461cb385b9d777d06138841e573acfee80ad416
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2007-01-11 12:38:36 +0000

    python-libs - new layout

--- python-libs/um_config.py
+++ python-libs/um_config.py
@@ -1,103 +1,128 @@
-# Global configuration
+# global informations
+svnrepos    = "svn://mesh.umic.rwth-aachen.de/umic-mesh"
 imageprefix = "/opt/umic-mesh/images"
-svnprefix = "/opt"
+svnprefix 	= "/opt/checkout"
 
-
-# Startup configuration
-# The keys in this dicts are allowed to be mrouter*, mclient*, meshrouter and meshclient
-startupinfos = dict (
-	meshrouter = ['execpy("/usr/local/bin/um_madwifi.py",["--debug","autocreate"])',
-				  'startdaemon("watchdog")'],
-	goldfinger = ['startdaemon("watchdog")']
-)
-
-daemoninfos = dict (
-	watchdog = dict (
-	                  path = "/usr/local/bin/um_watchdog.sh",
-					  args = []
-	)
-)
-
-# Common Wireless configuration
-wlaninfos =	dict (
-	wifi0 = dict (  essid    = "umic-mesh",
-					device   = "ath0",
-					channel  = "1",
-					antenna  = 2,
-					address  = "169.254.9.$NODENR/16",
-					wlanmode = "ahdemo",
-					txpower   = "20"					
-				  )
-)
-
-# Information about the kernel
-kernelinfos = dict (
+# informations about the kernel
+kernelinfos = dict(
 	mirror = "http://sunsite.informatik.rwth-aachen.de/ftp/pub/Linux/kernel/",
 	version = "2.6.16.29",
 	srcpath = "/usr/src",
-	modifiedfiles = ('include/net/ip_fib.h','include/net/route.h',
-					 'net/ipv4/fib_semantics.c','net/ipv4/route.c')
+	modifiedfiles = ['include/net/ip_fib.h','include/net/route.h',
+					 'net/ipv4/fib_semantics.c','net/ipv4/route.c']
 )
 
-# Information about olsr
-olsrinfos = dict (
+# informations about olsr
+olsrinfos = dict(
 	remote_repos    = ":pserver:anonymous@olsrd.cvs.sourceforge.net:/cvsroot/olsrd",
 	remote_module   = "olsrd-current",
 	local_upstream  = "/routing/olsr/branches/upstream",
 	local_trunk     = "/routing/olsr/trunk"
 )
 
-# Information about madwifi
-madwifiinfos = dict (
+# informations about madwifi
+madwifiinfos = dict(
 	remote_repos    = "http://svn.madwifi.org",
 	remote_module   = "/trunk",
 	local_upstream  = "/drivers/madwifi-ng/branches/upstream",
 	local_trunk     = "/drivers/madwifi-ng/trunk"
 )
 
-# Information about the subversion repository
-svninfos = dict (
-	svnrepos  = "svn://goldfinger.informatik.rwth-aachen.de/mcg-mesh",
-	svnmappings = { '/routing/olsr/branches/olsr4-mcg' : '/routing/olsr4',
-					'/routing/olsr/branches/olsr5-mcg' : '/routing/olsr5',
-					'/routing/aodv/trunk' : '/routing/aodv',
-					'/routing/dymo/trunk' : '/routing/dymo',
-					'/scripts' : '/scripts',
-					'/tools/dbttcp/trunk' : '/tools/dbttcp',
-					'/tools/nuttcp/trunk' : '/tools/nuttcp'
-					},
-	svnmappings_meshnode = {
-					'/config/meshnode/trunk' : '/config',
-					'/linux/vanilla/trunk' : '/linux-trunk',
-					'/drivers/madwifi-ng/branches/mcg-version' : '/drivers/madwifi-ng'
-	},
-	svnmappings_vmeshnode = {
-					'/config/meshnode/trunk' : '/config',
-					'/linux/xen/trunk' : '/linux-trunk'
-	},
-	svnmappings_vmeshhost = {
-					'/config/vmeshhost/trunk' : '/config',
-					'/linux/xen/trunk' : '/linux-trunk'
-	}
+# informations about the wlan devices
+wlaninfos = dict(
+    ath0 = dict(
+        wlandev  = "wifi0",
+        essid    = "umic-mesh",
+        channel  = "1",
+        antenna  = "2",
+        address  = "169.254.9.$NODENR/16",
+        wlanmode = "ahdemo",
+        txpower  = "20"
+    ),
+    ath1 = dict(
+        wlandev  = "wifi1",
+        essid    = "umic-mesh",
+        channel  = "11",
+        antenna  = "2",
+        address  = "169.254.10.$NODENR/16",
+        wlanmode = "sta",
+        txpower  = "20"
+    )
 )
 
-# UMIC-Mesh Node Information
+# informations about the daemons
+daemoninfos = dict(
+    watchdog = dict(
+        path = "/usr/local/bin/um_watchdog",
+        args = []
+    )
+)
+
+# informations about the umic-mesh nodes
 nodeinfos = dict(
+	vmeshhost = dict(
+		hostprefix  = 'vmeshhost',
+		wlandev     = [],
+		startup     = [],
+		daemons		= []
+	),
 	vmeshnode = dict(
-		hostprefix = 'vmeshnode'
+		hostprefix  = 'vmeshnode',
+		wlandev     = [],
+		startup     = [],
+        daemons     = []
 	),
 	meshnode = dict(
 	    hostprefix 	= 'meshrouter',
-		wlandev	    = "ath0"
-	),
-	vmeshhost = dict(
-		hostprefix = 'vmeshhost'
+		wlandev	    = ['ath0', 'ath1'],
+		startup     = ['execpy("/usr/local/bin/um_madwifi",["--debug","autocreate"])'],
+        daemons     = []
 	)
 )
 
-# Informations about the different images
+# informations about the different images
 imageinfos = dict(
-	ubuntu = dict(
-	    mounts = {}
-	)
+    vmeshhost = dict(
+		default  = 'um_edgy',
+		versions = ['um_edgy'],
+		mounts = {},
+		svnmappings = { '/config/vmeshhost/trunk' : '/config',
+						'/linux/xen/trunk' : '/linux/default',
+						'/scripts/python-libs' : '/scripts/python-libs'}
+	),
+	vmeshnode = dict(
+        default  = 'um_edgy',
+        versions = ['um_edgy'],
+		mounts = {},
+		svnmappings = { '/config/vmeshnode/trunk' : '/config',
+						'/linux/xen/trunk' : '/linux/default',
+						'routing/olsr/branches/um-version-olsr4' : '/routing/olsr4',
+						'/routing/olsr/branches/um-version-olsr5' : '/routing/olsr5',
+						'/scripts/mesh-init' : '/scripts/mesh-init',
+						'/scripts/mesh-stat' : '/scripts/mesh-stat',
+						'/scripts/python-libs' : '/scripts/python-libs',
+						'/tools/nuttcp/trunk' : '/tools/nuttcp'}
+	),
+	meshnode = dict(
+        default  = 'um_edgy',
+        versions = ['um_dapper', 'um_edgy'],
+		mounts = {},
+        svnmappings = { '/config/vmeshnode/trunk' : '/config',
+                        '/drivers/madwifi-ng/branches/um-version' : '/drivers/madwifi-ng',
+                        '/linux/vanilla/trunk' : '/linux/default',
+                        '/routing/olsr/branches/um-version-olsr4' : '/routing/olsr4',
+                        '/routing/olsr/branches/um-version-olsr5' : '/routing/olsr5',
+                        '/scripts/mesh-init' : '/scripts/mesh-init',
+                        '/scripts/mesh-stat' : '/scripts/mesh-stat',
+                        '/scripts/python-libs' : '/scripts/python-libs',
+                        '/tools/nuttcp/trunk' : '/tools/nuttcp'}
+    )
 )
+
+# informations about the different projects
+projectinfos = dict(
+        elcn = dict(
+            svnmappings = { '/linux/xen/trunk' : '/linux/elcn'}
+        )
+)
+
--- python-libs/um_util.py
+++ python-libs/um_util.py
@@ -2,123 +2,122 @@
 # -*- coding: utf-8 -*-
 
 # python imports
-import subprocess,types,os,sys,re,copy
+import subprocess, os, sys
 from logging import info, debug, warn, error
-from socket import gethostname
 
 # umic-mesh imports
 from um_config import *
 
-
-# Generic helper functions + classes
+#
+# generic helper functions and classes
+#
 
 class CommandFailed(Exception):
-	"Framework for UMIC-Mesh applications"
-	
+	"Convenience function to handle returncodes"
 	
 	def __init__(self, cmd, rc, stderr = None):
 		self.cmd = cmd
 		self.rc  = rc
 		self.stderr = stderr
 	
-		
 	def __str__(self):
 		return "Command %s failed with return code %d." %(self.cmd, self.rc)
 
 
-
 def execute(cmd, shell, raiseError=True):
-	"Convenience function to handle returncodes"
+	"Excecute a shell command"
 
 	debug("Executing: %s" % cmd.__str__())
-	prog = subprocess.Popen(cmd,shell = shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	prog = subprocess.Popen(cmd, shell = shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	(stdout, stderr) = prog.communicate()
 	rc = prog.returncode
 	if raiseError and rc != 0:
 		raise CommandFailed(cmd, rc, stderr)
+	
 	return (stdout, stderr)
 
 
 def call(cmd, shell, raiseError=True):
-	"Convenience function to handle returncodes"
+	"Call a shell command"
 
 	debug("Executing: %s" % cmd.__str__())
-	rc = subprocess.call(cmd,shell = shell)
+	rc = subprocess.call(cmd, shell = shell)
 	if raiseError and rc != 0:
 		raise CommandFailed(cmd, rc)
 
 
+#
+# mesh specific helper functions and classes
+#
 
-# Mesh specific helper functions + classes
-
-# check environment variable MESH and return nodetype
 def getnodetype():
-	global nodetype,nodeinfo
+	"Check environment variable UM_NODE_TYPE and return nodetype and nodeinfo"
 
-	# do action only if not yet registered
+	global nodetype, nodeinfo
+
 	if not globals().has_key('nodetype'):
-		if os.environ.has_key('MESH') and nodeinfos.has_key(os.environ['MESH']):
-			nodetype = os.environ['MESH']
+		if os.environ.has_key('UM_NODE_TYPE') and nodeinfos.has_key(os.environ['UM_NODE_TYPE']):
+			nodetype = os.environ['UM_NODE_TYPE']
 			nodeinfo = nodeinfos[nodetype]
 		else:
-			error("please set environment variable MESH to one of %s" % nodeinfos.keys())
+			error("Please set environment variable UM_NODE_TYPE to one of %s." % nodeinfos.keys())
 			sys.exit(1)
 
-	return (nodetype,nodeinfo)
+	return (nodetype, nodeinfo)
 
 
-# check environment variable MESH_IMAGE and return nodetype
-def getimagetype():
-	global imagetype,imageinfo
+def getimageinfo():
+	"Check environment variable UM_NODE_TYPE and return imageinfo"
 
-	if not globals().has_key('imagetype'):
-#		info("registering imagetype")
-		if os.environ.has_key('MESH_IMAGE') and imageinfos.has_key(os.environ['MESH_IMAGE']):
-			imagetype = os.environ['MESH_IMAGE']
-			imageinfo = imageinfos[imagetype]
-			if globals().has_key('nodetype'):
-				imagepath = "%s/%s/%s" % (imageprefix,imagetype,nodetype)
+	global imageinfo
+
+	if not globals().has_key('imageinfo'):
+		(nodetype, nodeinfo) = getnodetype()
+
+		if imageinfos.has_key(nodetype):
+			imageinfo = imageinfos[nodetype]
 		else:
-			error("please set environment variable MESH_IMAGE to one of %s" % imageinfos.keys())
+			error("No image defined for UM_NODE_TYPE=%s. Please set environment variable " \
+				  "UM_NODE_TYPE to one of %s." %(nodetype, imageinfos.keys()))
 			sys.exit(1)
 
-	return (imagetype,imageinfo)
+	return imageinfo
 
 
-# get imagepath
+def getimageversion():
+	"Check environment variable UM_IMAGE_VERSION and return imageversion"
+
+	global imageversion
+
+	if not globals().has_key('imageversion'):
+		imageinfo = getimageinfo()
+
+		if os.environ.has_key('UM_IMAGE_VERSION') and os.environ['UM_IMAGE_VERSION'] in imageinfo['versions']:
+			imageversion = os.environ['UM_IMAGE_VERSION']
+		else:
+			error("No image version defined for UM_NODE_TYPE=%s. Please set environment variable " \
+				  "UM_IMAGE_VERSION to one of %s." %(nodetype, imageinfo['versions']))
+			sys.exit(1)
+
+	return imageversion
+
+
 def getimagepath():
+	"Derivate image path form environment variables UM_NODE_TYPE and UM_IMAGE_VERSION"
+
 	global imagepath
 	
 	if not globals().has_key('imagepath'):
-		(nodetype,nodeinfo) = getnodetype();
-	#	(imagetype,imageinfo) = getimagetype();
-	#	imagepath = "%s/%s/%s" % (imageprefix,imagetype,nodetype)
-	imagepath = "%s/%s" % (imageprefix,nodetype)
-
+		imageversion = getimageversion()
+		imagepath = "%s/%s.img/%s" % (imageprefix, nodetype, imageversion)
+	
 	return imagepath
 
 
-# check if user is root
 def requireroot():
+	"Check if user is root"
+
 	if not os.getuid()==0:		
-		error("you must be root to do this")
+		error("You must be root to do this!")
 		sys.exit(1)
-	
-
-# get nodename from nodenumber
-def nodename(nr):
-	requirenodetype()
-	
-	if type(nr) is types.StringType: return nr
-	return "%s%d" % (nodeinfo['hostprefix'],nr)
-
-
-# get nodenumber
-def getnodenr():
-	"Get node number from hostname"
-	
-	hostname = gethostname()
-	nr = re.sub("^mrouter","",hostname)
-	return nr
-
 
