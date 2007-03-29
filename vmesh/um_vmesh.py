3a6936990ba815f621347a187567448103d9aed2
Lars Noschinski <lars.noschinski@rwth-aachen.de>
2007-03-29 09:19:05 +0000

    Removed some testing code (and use a correct address therefore)

--- vmesh/um_vmesh.py
+++ vmesh/um_vmesh.py
@@ -142,7 +142,7 @@ colon reaches host1. Empty lines and lines starting with # are ignored.
 
     def public_ip(self):
         """ Gets the local public ip """
-        return "127.0.0.10"
+        return socket.gethostbyname(gethostname))
 
     def setup_gre(self):
         hostnum = self.node.number()
