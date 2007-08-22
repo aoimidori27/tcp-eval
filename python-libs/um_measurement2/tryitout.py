e8b6161065491c4e122300ab9d70a9f92620468f
Lars Noschinski <lars.noschinski@rwth-aachen.de>
2007-08-22 14:00:54 +0000

    Example usage of sshexec.

---
+++ python-libs/um_measurement2/tryitout.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from twisted.internet import defer, reactor
+
+from sshexec import SSHConnectionFactory
+
+@defer.inlineCallbacks
+def main():
+    scf = SSHConnectionFactory("noschinski")
+    yield scf.connect(["localhost","127.0.0.1"])
+    result = yield scf.remoteExecute("localhost", "ls", out_fd=sys.stdout, err_fd=sys.stderr)
+    print "RESULT: %s" % result
+    result = yield scf.remoteExecute("localhost", "echo ----------------------; whoami", out_fd=sys.stdout, err_fd=sys.stderr)
+    print "RESULT: %s" % result
+    yield scf.disconnect()
+    reactor.stop()
+
+
+main()
+reactor.run()
