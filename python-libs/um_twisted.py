3dfae9af04bc979a9835dd2e49e3d873cc244997
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2007-09-05 17:25:31 +0000

    scripts: added module um_twisted with general purpose functions for the twisted framework

---
+++ python-libs/um_twisted.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python2.5
+# -*- coding: utf-8 -*-
+
+
+import os
+
+from twisted.internet import defer, utils
+
+
+"""
+
+This module contains useful functions to use within the twisted framework.
+
+
+"""
+
+            
+
+@defer.inlineCallbacks
+def twisted_execute(cmd, shell=True):
+    """
+    Executes the given command and
+    returns a sequence (stdout, stderr, returncode)
+    
+    Function which mimics um_functions.execute()
+    but without exceptions.
+    
+    for use in twisted (the subprocess module won't work!!!)
+
+    """
+    debug("Executing: %s" % cmd.__str__())
+    if not shell:
+        (stdout, stderr, rc) = yield _ExecuteHelper(cmd[0],
+                                                    cmd[1:],
+                                                    env=os.environ,
+                                                    path='.')
+    else:
+        (stdout, stderr, rc) = yield _ExecuteHelper("/bin/sh",
+                                                    ('-c', cmd),
+                                                    env=os.environ,
+                                                    path='.')
+        
+    defer.returnValue((stdout, stderr, rc))
+
+@defer.inlineCallbacks
+def twisted_call(cmd, shell=True):
+    """
+    Executes the given command and
+    returns a returncode suppressing output.
+
+    Warning: is inefficient for commands with large stdoutput!
+    """
+    (stdout, stderr, rc) = yield twisted_execute(cmd, shell)
+
+    defer.returnValue(rc)
+
+class _ExecuteHelper(defer.Deferred):
+    """
+    
+    This a wrapper around utils.getProcessOutputAndValue, which returns the
+    signal number as negative returncode in case of errback()
+    
+    """
+
+    def __init__(self, *args, **kwargs):
+        defer.Deferred.__init__(self)
+        d = utils.getProcessOutputAndValue(*args, **kwargs)
+        d.addCallbacks(self.callback, self._internal_errback)
+
+    def _internal_errback(self, failure):
+        (stdout, stderr, signum) = failure.value
+        self.callback((stdout, stderr, -signum))
