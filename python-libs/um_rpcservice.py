85f33c5ebbcf11c5be264eb328ef428ea390d681
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2007-09-14 16:22:39 +0000

    added multiline error logging

--- python-libs/um_rpcservice.py
+++ python-libs/um_rpcservice.py
@@ -20,16 +20,22 @@ class RPCService(xmlrpc.XMLRPC):
 
     # to make logging more meaningful use own logging methods!
     def info(self, msg, *args, **kwargs):
-        logging.info("%s%s" %(self._name, msg), *args, **kwargs)
+        logging.info("%s:%s" %(self._name, msg), *args, **kwargs)
 
     def debug(self, msg, *args, **kwargs):
-        logging.debug("%s%s" %(self._name, msg), *args, **kwargs)
+        logging.debug("%s:%s" %(self._name, msg), *args, **kwargs)
 
     def warn(self, msg, *args, **kwargs):
-        logging.warn("%s%s" %(self._name, msg), *args, **kwargs)
+        logging.warn("%s:%s" %(self._name, msg), *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        logging.error("%s%s" %(self._name, msg), *args, **kwargs)
+        logging.error("%s:%s" %(self._name, msg), *args, **kwargs)
         
     def critical(self, msg, *args, **kwargs):
-        logging.critical("%s%s" %(self._name, msg), *args, **kwargs)
+        logging.critical("%s:%s" %(self._name, msg), *args, **kwargs)
+
+
+    def stderror(self, multilinemsg):
+        """ Prints a multiline error message with indentation """
+        for line in multilinemsg.splitlines():
+            self.error(" %s" %line)
