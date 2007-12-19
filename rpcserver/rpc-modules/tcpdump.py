96e6b919a5b6176202767741cda38b50fdd126a5
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2007-12-19 13:44:36 +0000

    fixed bug in debug output

--- rpcserver/rpc-modules/tcpdump.py
+++ rpcserver/rpc-modules/tcpdump.py
@@ -85,7 +85,7 @@ class Tcpdump(xmlrpc.XMLRPC):
             defer.returnValue(None)
 
         self._proc = _TcpdumpProtocol()
-        debug("spawnProcess: cmd=%s args=%s" %(self._proc, args))
+        debug("spawnProcess: %s" %cmd)
         reactor.spawnProcess(self._proc, self._daemon, args = cmd, path='/',
                 childFDs = {1: temp_fd, 2: "r"})
         os.close(temp_fd)
