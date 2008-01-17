5b52232aa5b784f732efc373e44a4e8f1fb8d914
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2008-01-17 10:21:25 +0000

    Remove old backup file

--- backup/postgresql-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-umask 027
-BACKUP_DIR=${1:-/backup/postgresql}
-BACKUP_FILE="pg_dumpall"
-
-if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
-su postgres pg_dumpall | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
