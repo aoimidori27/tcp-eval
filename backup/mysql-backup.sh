c65edb83c29dae36c7fac4522733ba7614ee884c
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2008-08-26 16:22:47 +0000

    change default dump directory and make rsyncable gzips

--- backup/mysql-backup.sh
+++ backup/mysql-backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 umask 027
-BACKUP_DIR=${1:-/backup/mysql}
+BACKUP_DIR=${1:-/dumps/mysql}
 BACKUP_FILE="mysql_dumpall"
 
 if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
-/usr/bin/mysqldump --all-databases | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
+/usr/bin/mysqldump --all-databases | gzip --rsyncable > $BACKUP_DIR/$BACKUP_FILE.gz
