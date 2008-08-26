b50c446f6b737b29541ba34762b98aab4c422e56
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2008-08-26 15:46:54 +0000

    changed subversion backup script to perform incremental backups

--- backup/subversion-backup.sh
+++ backup/subversion-backup.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
 umask 027
-BACKUP_DIR=${1:-/backup/subversion}
+BACKUP_DIR=${1:-/dumps/subversion}
+
+# time in seconds at which a FULL dump is regenerated (30 days)
+FULL_EXPIRE=$((3600*24*30))
+NOW=$(/bin/date "+%s")
 
 if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
+
 for i in /srv/svn/*; do
-    BACKUP_FILE=`basename "$i"`
-    svnadmin dump "$i" -q | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
+    REV=$(svnlook youngest $i)
+    BACKUP_FILE_FULL=$BACKUP_DIR/$(basename "$i")_dump_full.gz
+    BACKUP_FILE_INCR=$BACKUP_DIR/$(basename "$i")_dump_incr.gz
+    BACKUP_FILE_FULL_REV=$BACKUP_DIR/$(basename "$i")_FULL_REV
+    LAST_FULL_CHANGE=0
+    if [ -e "$BACKUP_FILE_FULL" -a -e "$BACKUP_FILE_FULL_REV" ]; then
+      LAST_FULL_CHANGE=$(stat --format="%X" $BACKUP_FILE_FULL);
+      LAST_FULL_REV=$(cat $BACKUP_FILE_FULL_REV)
+	fi;
+    EXPIRY=$(($LAST_FULL_CHANGE+$FULL_EXPIRE))
+
+    if [ $EXPIRY -lt $NOW ]; then
+	  # make full dump
+      svnadmin dump "$i" -r 0:$REV -q | gzip > $BACKUP_FILE_FULL
+      echo $REV > $BACKUP_FILE_FULL_REV
+    else
+  	  # only incremental dump 
+      OLDREV=$(($LAST_FULL_REV+1))
+      if [ $OLDREV -le $REV ]; then
+        svnadmin dump "$i" -r $(($LAST_FULL_REV+1)):$REV --incremental -q | gzip > $BACKUP_FILE_INCR
+      fi
+    fi; 
 done
