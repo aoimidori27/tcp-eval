c085398c33384e70885b9d1c054a6b99d09653bc
Lennart Schulte <lennart.schulte@rwth-aachen.de>
2008-11-20 15:42:36 +0000

    minor changes

--- python-libs/um_measurement/tests.py
+++ python-libs/um_measurement/tests.py
@@ -337,7 +337,7 @@ def test_flowgrind_dd(mrs,
 
     dst_ip = yield mrs.getIp(dst.getHostname(), flowgrind_iface)
 
-    cmd = "flowgrind-dd -O s=TCP_CONG_MODULE=%s -T s=%.3f -p %u -H %s/%s" % (flowgrind_cc,
+    cmd = "flowgrind_dd -O s=TCP_CONG_MODULE=%s -T s=%.3f -p %u -H %s/%s" % (flowgrind_cc,
                                                          flowgrind_duration,
                                                          flowgrind_bport,
                                                          dst_ip,
