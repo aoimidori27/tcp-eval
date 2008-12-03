3c3b179fdec54c601de856cfa0b939523b06884d
Lennart Schulte <lennart.schulte@rwth-aachen.de>
2008-12-03 10:17:42 +0000

    added versions of tcp_analysis and flow_plotter for new flowgrind distributed

---
+++ analysis/flow_plotter_dd.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+from logging import info, debug, warn, error
+
+from um_analysis.testrecords_flowgrind_dd import FlowgrindddRecordFactory
+from um_gnuplot import UmHistogram, UmGnuplot, UmLinePlot, UmBoxPlot
+from um_application import Application
+
+class FlowPlotter(Application):
+    
+    def __init__(self):
+
+        Application.__init__(self)
+
+        # object variables
+        self.factory = FlowgrindddRecordFactory()
+
+        # initialization of the option parser
+        usage = "usage: %prog [options] file [file]..." 
+
+        self.parser.set_usage(usage)
+        self.parser.set_defaults(outdir = "./")
+        self.parser.add_option('-O', '--output', metavar="OutDir",
+                        action = 'store', type = 'string', dest = 'outdir',
+                        help = 'Set outputdirectory [default: %default]')        
+        self.parser.add_option("-c", "--cfg", metavar = "FILE",
+                        action = "store", dest = "cfgfile",
+                        help = "use the file as config file for LaTeX. "\
+                               "No default packages will be loaded.")        
+        
+    def set_option(self):
+        "Set options"
+        Application.set_option(self)
+
+        if len(self.args) < 1:
+            error("no input files, stop.")
+            sys.exit(1)
+        
+        if not os.path.exists(self.options.outdir):
+            info("%s does not exist, creating. " % self.options.outdir)
+            os.mkdir(self.options.outdir)
+
+    def plot(self, infile, outdir=None):
+        def ssth_max(ssth, cwnd_max, x):
+            if ssth > cwnd_max+x:
+                return cwnd_max+x
+            else:
+                return ssth
+
+        if not outdir:
+            outdir=self.options.outdir
+        record = self.factory.createRecord(infile, "flowgrinddd")
+        flows = record.calculate("flows")
+        
+        plotname = os.path.splitext(os.path.basename(infile))[0]    
+        valfilename = os.path.join(outdir, plotname+".values")
+        
+        info("Generating %s..." % valfilename)
+        fh = file(valfilename, "w")
+
+        flow = flows[0]
+
+        #get max cwnd for ssth output
+        cwnd_max = 0
+        for i in range(flow['S'].size):
+            if flow['S'].cwnd[i] > cwnd_max:
+                cwnd_max = flow['S'].cwnd[i]
+            if flow['R'].cwnd[i] > cwnd_max:
+                cwnd_max = flow['R'].cwnd[i]
+
+        # header
+        fh.write("# start_time end_time forward_tput reverse_tput\n")
+        for i in range(flow['S'].size):
+            fh.write("%f %f %f %f %f %f %f %f %f\n" %(flow['S'].begin[i], flow['S'].end[i],
+                                                   flow['S'].tput[i], flow['R'].tput[i],
+                                                   flow['S'].cwnd[i], flow['R'].cwnd[i], 
+                                                   ssth_max(flow['S'].ssth[i], cwnd_max, 50),
+                                                   flow['S'].krtt[i], flow['S'].krto[i]))
+        fh.close()
+        
+        # tput
+        p = UmLinePlot(plotname+'_tput')
+        p.setYLabel(r"$\\Mbps$")
+        p.setXLabel("time")
+        p.plot(valfilename, "Throughput", using="2:3", linestyle=2)
+        p.plot(valfilename, "reverse Throughput", using="2:4", linestyle=3)
+        # output plot
+        p.save(self.options.outdir, self.options.debug, self.options.cfgfile)
+
+        # cwnd
+        p = UmLinePlot(plotname+'_cwnd_ssth')
+        p.setYLabel(r"$\\#$")
+        p.setXLabel("time")
+        p.plot(valfilename, "Sender CWND", using="2:5", linestyle=2)
+        p.plot(valfilename, "Receiver CWND", using="2:6", linestyle=3)
+        p.plot(valfilename, "SSTH", using="2:7", linestyle=4)
+        # output plot
+        p.save(self.options.outdir, self.options.debug, self.options.cfgfile)
+
+        # rto, rtt
+        p = UmLinePlot(plotname+'_rto_rtt')
+        p.setYLabel(r"$\\ms$")
+        p.setXLabel("time")
+        p.plot(valfilename, "RTO", using="2:8", linestyle=2)
+        p.plot(valfilename, "RTT", using="2:9", linestyle=3)
+        # output plot
+        p.save(self.options.outdir, self.options.debug, self.options.cfgfile)
+
+        
+    def run(self):
+        """ Run... """
+
+        for infile in self.args:
+            self.plot(infile)
+
+    
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    inst=FlowPlotter()
+    inst.parse_option()
+    inst.set_option()
+    inst.run()
+        
+
