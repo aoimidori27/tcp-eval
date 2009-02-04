3005f4ead7f86733793d18f697d7ba6596f76028
Arnd Hannemann <arnd.hannemann@rwth-aachen.de>
2009-02-04 17:22:44 +0000

    add service to start the routemonitor

---
+++ rpcserver/rpc-modules-meshnode/routemonitor.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python2.5
+# -*- coding: utf-8 -*-
+
+import os
+from logging import info, debug, warn, error, critical
+from tempfile import mkstemp
+
+# twisted imports
+from twisted.internet import defer, threads, protocol, utils
+from twisted.web import xmlrpc
+
+from um_rpcservice import RPCService
+from um_twisted_functions import twisted_execute, twisted_call
+          
+
+class Route_monitor(RPCService):
+    """Class for managing the route monitor daemon"""
+
+
+    #
+    # Public XMLRPC Interface
+    #
+
+    @defer.inlineCallbacks
+    def xmlrpc_restart(self):
+        rc = yield self.reread_config()
+        
+        if rc == 0:
+            # don't complain if stopping doesnt work
+            yield self.stop()
+            rc = yield self.start()
+        defer.returnValue(rc)
+
+    @defer.inlineCallbacks
+    def xmlrpc_start(self):
+        rc = yield self.reread_config()
+        if rc == 0:            
+            rc = yield self.start()
+        defer.returnValue(rc)
+
+    def xmlrpc_stop(self):
+        return self.stop()
+
+    @defer.inlineCallbacks
+    def xmlrpc_isAlive(self):
+        """ Check if the routemonitor is alive by looking in the process table. """
+        cmd = ["/bin/ps", "-C", "routemonitor" ]
+        rc = yield twisted_call(cmd, shell=False)
+
+        if (rc==0):
+            defer.returnValue(True)
+        else:
+            defer.returnValue(False)
+
+
+    #
+    # Internal stuff
+    #
+    def __init__(self, parent = None):
+        
+        # Call super constructor
+        RPCService.__init__(self, parent)
+
+        self._name = "routemonitor"
+        self._config = None
+        self._daemon = "/usr/local/sbin/routemonitor"
+        self._pidfile = "/var/run/routemonitor.pid"
+    
+    
+    @defer.inlineCallbacks
+    def reread_config(self):
+        """ Rereads the configuration
+
+            The routemonitor service table has the following important columns
+                 sink_host : the host to send the SNMP informs to
+                 sink_port : the port to send the SNMP informs to
+                 sink_iface : the interface to monitor for route changes
+
+            Will return 0 on success.
+            
+        """
+        
+        assoc = yield self._parent._dbpool.getCurrentServiceConfig(self._name)
+        if not assoc:
+            info("Found no configuration")
+            defer.returnValue(-1)
+
+        self._config = assoc
+
+        defer.returnValue(0)
+                                   
+
+    @defer.inlineCallbacks
+    def start(self):
+        """ This function invokes start-stop daemon to bring up the route monitor"""
+        
+        args = [ '-h', self._config['sink_host'],
+                 '-p', self._config['sink_port'] ]
+        if self._config['interface']:
+            args.extend(['-i', self.config['interface'])
+        cmd = [ "start-stop-daemon", "--start",  
+                "--pidfile", self._pidfile,
+                "--exec", self._daemon,
+                "--"]
+        cmd.extend(args)
+        (stdout, stderr, rc) = yield twisted_execute(cmd, shell=False)
+        if len(stdout):
+            debug(stdout)
+        if (rc != 0):
+            error("routemonitor.start(): Command failed with RC=%s", rc)
+            for line in stderr.splitlines():
+                error(" %s" %line)
+            # when an error occurs stdout is important too
+            if len(stdout):
+                stderr = stderr+stdout
+
+        yield self._parent._dbpool.startedService(self._config,
+                                                  rc, message=stderr)
+        defer.returnValue(rc)
+
+    @defer.inlineCallbacks
+    def stop(self):
+        """ This function invokes start-stop-daemon to stop routemonitor"""
+
+        cmd = [ "start-stop-daemon", "--stop",  "--quiet",
+                "--exec", self._daemon,
+                "--pidfile", self._pidfile,
+                "--signal", "TERM",
+                "--retry",  "5"]
+        (stdout, stderr, rc) = yield twisted_execute(cmd, shell=False)
+        if len(stdout):
+            debug(stdout)
+        if (rc != 0):
+            error("routemonitor.stop(): Command failed with RC=%s", rc)
+            for line in stderr.splitlines():
+                error(" %s" %line)
+        yield self._parent._dbpool.stoppedService(self._config,
+                                                  rc, message=stderr)            
+        defer.returnValue(rc)
