c3d1cc0f00e7b276f20291a5bb81c33d3d769ac2
Alexander Zimmermann <alexander.zimmermann@rwth-aachen.de>
2010-02-04 16:47:41 +0000

    Update draft2pdf

--- pdfconverters/draft2pdf.py
+++ pdfconverters/draft2pdf.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # python imports
-import os
-import subprocess
+import os.path
 from logging import info, debug, warn, error
 
 # umic-mesh imports
 from um_application import Application
+from um_functions import call, execute
+
 
 class Draft2Pdf(Application):
-    """Class to convert internet draft text files to pdf"""
+    """Class to convert internet draft text files to pdfs"""
 
     def __init__(self):
         """Constructor of the object"""
@@ -18,55 +19,83 @@ class Draft2Pdf(Application):
         Application.__init__(self)
 
         # initialization of the option parser
-        usage = "usage: %prog [options] <file1> <file2> .."
+        usage = "usage: %prog [options] <file1> <file2> ..."
         self.parser.set_usage(usage)
-        self.parser.set_defaults(force = False, textsuffix = 'txt', pdfsuffix = 'pdf')
+        self.parser.set_defaults(force = False, outdir = os.getcwd())
 
         self.parser.add_option("-f", "--force",
                                action = "store_true", dest = "force",
                                help = "overwrite existing output pdf file")
-        self.parser.add_option("-t", "--textIn", metavar = "SUF",
-                               action = "store", dest = "textsuffix",
-                               help = "define suffix of input text files [default: %default]")
-        self.parser.add_option("-p", "--pdfOut", metavar = "SUF",
-                               action = "store", dest = "pdfsuffix",
-                               help = "define suffix of output pdf file [default: %default]")
+        self.parser.add_option("-n", "--name", metavar = "NAME",
+                               action = "store", dest = "basename",
+                               help = "basename for all generated pdf files")
+        self.parser.add_option("-d", "--directory", metavar = "DIR",
+                               action = "store", dest = "outdir",
+                               help = "output directory [default: %default]")
+
+    def set_option(self):
+        """Set options"""
+
+        Application.set_option(self)
+
+        # correct numbers of arguments?
+        if len(self.args) == 0:
+            self.parser.error("incorrect number of arguments")
 
     def run(self):
         """Main method of the DraftToPdf object"""
+        
+        # get all necessary directories
+        srcdir = os.getcwd()
+        destdir = self.options.outdir
+                       
+	    # for all ID given on command line
+        for index, draft in enumerate(self.args):
 
-	    # process every file given on command line
-        for entry in self.args:
-            if not os.path.isfile(entry):
-                warn("%s is not a regular file. Skipped." %entry)
+            # get the full path of the figure
+            draftSrc = os.path.join(srcdir, draft)
+            
+            if not os.path.isfile(draftSrc):
+                warn("%s is not a regular file. Skipped." %draft)
                 continue
 
-            pdfoutput = "%s.%s" %(os.path.splitext(os.path.basename(entry))[0],
-                                  self.options.pdfsuffix)
-
-            if not self.options.force and os.path.exists(pdfoutput):
-                warn("%s already exists. Skipped." %pdfoutput)
-                continue
-
-            # convert the text file to a PDF file
-            if self.options.debug:
-                cmd1 = ("enscript", "-v", "-B", "-f", "Courier10",
-                        "--margins=70::50:", entry, "-p", "-")
+            # get the basename (without extension)
+            if self.options.basename:
+                basename = "%s_%s" %(self.options.basename, index)
             else:
-                cmd1 = ("enscript", "-q", "-B", "-f", "Courier10",
-                        "--margins=70::50:", entry, "-p", "-")
+                basename = os.path.basename(draft)
+                basename = os.path.splitext(basename)[0]
+           
+            # build pdf graphics
+            draftDst = os.path.join(destdir, "%s.pdf" %basename)
 
-            cmd2 = ("ps2pdf", "-", pdfoutput)
+            if not self.options.force and os.path.exists(draftDst):
+                warn("%s already exists. Skipped." %draftDst)
+                continue
 
-            enscript = subprocess.Popen(cmd1, stdout = subprocess.PIPE)
-            pstopdf = subprocess.Popen(cmd2, stdin = enscript.stdout)
-            (stdout, stderr) = pstopdf.communicate()
+            # convert the draft to a PS file
+            if self.options.debug:
+                cmd = ("enscript -v -B -f Courier10 --margins=70::50 %s -p -" %draftSrc)
+            else:
+                cmd = ("enscript -B -f Courier10 --margins=70::50 %s -p -" %draftSrc)
+
+            # we want only stdout
+            info("Run enscript on %s..." %draft)
+            enscript = execute(cmd)[0]
+
+            # convert the PS file to a PDF file
+            info("Run ps2pdf on %s..." %draft)
+            cmd = ("ps2pdf - %s" %draftDst)
+            if self.options.debug:
+                call(cmd, input = enscript)
+            else:
+                call(cmd, input = enscript, noOutput = True)
 
     def main(self):
         self.parse_option()
         self.set_option()
         self.run()
 
+
 if __name__ == "__main__":
     Draft2Pdf().main()
-
