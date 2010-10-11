4ff2e355462f89bae23d9255418c97928356ce4e
Lennart Schulte <lennart.schulte@rwth-aachen.de>
2010-10-11 16:27:29 +0000

    some additional header variables, fairness plots, axis labeling in upper case

--- analysis/vmesh_dumbbell_flowgrind_analysis.py
+++ analysis/vmesh_dumbbell_flowgrind_analysis.py
@@ -58,14 +58,22 @@ class ReorderingAnalysis(Analysis):
                         action = "store_true", dest = "dry_run",
                         help = "Test the flowlogs only")
 
+        self.parser.add_option('-F', '--fairness',
+                        action = "store_true", dest = "fairness",
+                        help = "Plot fairness instead")
+
         self.plotlabels = dict()
-        self.plotlabels["bnbw"]    = r"bottleneck bandwidth [$\\si[per=frac,fraction=nice]{\\Mbps}$]";
-        self.plotlabels["qlimit"]  = r"bottleneck queue length [packets]";
-        self.plotlabels["rrate"]   = r"reordering rate [$\\si{\\percent}$]";
-        self.plotlabels["rdelay"]  = r"reordering delay [$\\si{\\milli\\second}$]";
-        self.plotlabels["rtos"]    = r"RTO retransmissions [$\\#$]";
-        self.plotlabels["frs"]     = r"fast retransmissions [$\\#$]";
-        self.plotlabels["thruput"] = r"throughput [$\\si[per=frac,fraction=nice]{\\Mbps}$]";
+        self.plotlabels["bnbw"]    = r"Bottleneck Bandwidth [$\\si[per=frac,fraction=nice]{\\Mbps}$]";
+        self.plotlabels["qlimit"]  = r"Bottleneck Queue Length [packets]";
+        self.plotlabels["rrate"]   = r"Reordering Rate [$\\si{\\percent}$]";
+        self.plotlabels["rdelay"]  = r"Reordering Delay [$\\si{\\milli\\second}$]";
+        self.plotlabels["rtos"]    = r"RTO Retransmissions [$\\#$]";
+        self.plotlabels["frs"]     = r"Fast Retransmissions [$\\#$]";
+        self.plotlabels["thruput"] = r"Throughput [$\\si[per=frac,fraction=nice]{\\Mbps}$]";
+        self.plotlabels["fairness"]= r"Fairness"
+        self.plotlabels["delay"]   = r"RTT [$\\si{\\milli\\second}$]"
+        self.plotlabels["ackreor"] = r"ACK Reordering Rate [$\\si{\\percent}$]"
+        self.plotlabels["ackloss"] = r"ACK Loss Rate [$\\si{\\percent}$]"
 
 
     def set_option(self):
@@ -75,7 +83,7 @@ class ReorderingAnalysis(Analysis):
         if not self.options.variable:
             error("Please provide me with the variable and type of the measurement!")
             sys.exit(1)
-        if self.options.variable != "rrate" and self.options.variable != "rdelay" and self.options.variable != "qlimit" and self.options.variable != "bnbw":
+        if self.options.variable != "rrate" and self.options.variable != "rdelay" and self.options.variable != "qlimit" and self.options.variable != "bnbw" and self.options.variable != "delay" and self.options.variable != "ackreor" and self.options.variable != "ackloss":
             error("I did not recognize the variable you gave me!")
             sys.exit(1)
         if self.options.rotype != "reordering" and self.options.rotype != "congestion" and self.options.rotype != "both":
@@ -86,21 +94,39 @@ class ReorderingAnalysis(Analysis):
     def onLoad(self, record, iterationNo, scenarioNo, runNo, test):
         dbcur = self.dbcon.cursor()
 
-        recordHeader   = record.getHeader()
-        src            = recordHeader["flowgrind_src"]
-        dst            = recordHeader["flowgrind_dst"]
-        run_label      = recordHeader["run_label"]
-        scenario_label = recordHeader["scenario_label"]
-        variable       = recordHeader["testbed_param_variable"]
-        reordering     = recordHeader["testbed_param_reordering"]
-        qlimit         = int(recordHeader["testbed_param_qlimit"])
-        rrate          = int(recordHeader["testbed_param_rrate"])
-        rdelay         = int(recordHeader["testbed_param_rdelay"])
+        try:
+            recordHeader   = record.getHeader()
+            src            = recordHeader["flowgrind_src"]
+            dst            = recordHeader["flowgrind_dst"]
+            run_label      = recordHeader["run_label"]
+            scenario_label = recordHeader["scenario_label"]
+            variable       = recordHeader["testbed_param_variable"]
+            reordering     = recordHeader["testbed_param_reordering"]
+            qlimit         = int(recordHeader["testbed_param_qlimit"])
+            rrate          = int(recordHeader["testbed_param_rrate"])
+            rdelay         = int(recordHeader["testbed_param_rdelay"])
+        except:
+            return
 
         try:
             bnbw       = int(recordHeader["testbed_param_bottleneckbw"])
         except KeyError:
             bnbw       = "NULL"
+
+        try:
+            delay      = int(recordHeader["testbed_param_delay"])
+        except KeyError:
+            delay      = "NULL"
+
+        try:
+            ackreor    = int(recordHeader["testbed_param_ackreor"])
+        except KeyError:
+            ackreor    = "NULL"
+
+        try:
+            ackloss    = int(recordHeader["testbed_param_ackloss"])
+        except KeyError:
+            ackloss    = "NULL"
 
         # test_start_time was introduced later in the header, so its not in old test logs
         try:
@@ -117,6 +143,8 @@ class ReorderingAnalysis(Analysis):
             else:
                 self.failed[run_label] = self.failed[run_label]+1
             return
+        if thruput == 0:
+            warn("Throughput is 0 in %s!" %record.filename)
 
         try:
             rtos = int(rtos)
@@ -138,13 +166,76 @@ class ReorderingAnalysis(Analysis):
         if flow_S['end'][c] > 1:
             warn("Long connection establishment (%ss): %s" %(flow_S['end'][c], record.filename))
 
-
-
         dbcur.execute("""
-                      INSERT INTO tests VALUES ("%s", "%s", %s, %u, %u, %u, %s, %s, %u, %u, %u, "%s", "%s", %f, %u, "$%s$", "%s", "%s")
-                      """ % (variable, reordering, bnbw, qlimit, rrate, rdelay, rtos, frs, iterationNo, scenarioNo, runNo, src, dst, thruput,
+                      INSERT INTO tests VALUES ("%s", "%s", %s, %u, %u, %u, %u, %u, %u, %s, %s, %u, %u, %u, "%s", "%s", %f, %u, "$%s$", "%s", "%s")
+                      """ % (variable, reordering, bnbw, qlimit, delay, rrate, rdelay, ackreor, ackloss, rtos, frs, iterationNo, scenarioNo, runNo, src, dst, thruput,
                              start_time, run_label, scenario_label, test))
 
+    def generateFairnessOverXLinePlot(self):
+        """Generates a line plot of the DB column y over the DB column x
+           reordering rate. One line for each scenario.
+        """
+        y     = 'fairness'
+        x      = self.options.variable
+        rotype = self.options.rotype
+
+        dbcur = self.dbcon.cursor()
+
+        # get all scenario labels
+        dbcur.execute('''
+            SELECT DISTINCT scenarioNo
+            FROM tests ORDER BY scenarioNo'''
+        )
+        scenarios = []
+        for row in dbcur:
+            scen = row[0]
+            scenarios.append(scen)
+
+        outdir = self.options.outdir
+        p = UmLinePointPlot("%s_%s_over_%s" % (rotype, y, x))
+        p.setXLabel(self.plotlabels[x])
+        p.setYLabel(self.plotlabels[y])
+
+        for scenarioNo in scenarios:
+            query = '''
+                SELECT bnbw,avg(thruput),scenario_label
+                FROM tests
+                WHERE scenarioNo=%s
+                GROUP BY scenario_label, bnbw
+                ORDER BY bnbw;
+            ''' %scenarioNo
+            debug("\n\n" + query + "\n\n")
+            fairness = dbcur.execute(query).fetchall()
+
+            # fairness plot
+            k = 0
+            j = 1
+            plotname = "%s_%s_over_%s_s%u" % (rotype, y, x, scenarioNo)
+            valfilename = os.path.join(outdir, plotname+".values")
+
+            info("Generating %s..." % valfilename)
+            fhv = file(valfilename, "w")
+
+            # header
+            fhv.write("# %s %s\n" % (x, y))
+
+            # Jain's fairness index
+            for i in range(0, len(fairness), 2):
+                try:
+                    zaehler = (fairness[i][1] + fairness[i+1][1])**2
+                    nenner = 2 * (fairness[i][1]**2 + fairness[i+1][1]**2)
+                    jain_index = float(zaehler)/float(nenner)
+                except: jain_index = 0
+
+                fhv.write("%s %s\n" %(fairness[i][0], jain_index))
+
+            fhv.close()
+
+            p.plot(valfilename, "%s - %s" %(fairness[0][2], fairness[1][2]), linestyle=scenarioNo, using="1:2")
+
+        # make room for the legend
+        p.setYRange("[0.5:1.1]")
+        p.save(self.options.outdir, self.options.debug, self.options.cfgfile)
 
     def generateYOverXLinePlot(self, y):
         """Generates a line plot of the DB column y over the DB column x
@@ -234,7 +325,6 @@ class ReorderingAnalysis(Analysis):
 
         p.save(self.options.outdir, self.options.debug, self.options.cfgfile)
 
-
     def calculateStdDev(self, y, x_value, scenarioNo):
         """Calculates the standarddeviation for the values of the YoverXPlot
         """
@@ -270,8 +360,11 @@ class ReorderingAnalysis(Analysis):
                                 reordering  VARCHAR(15),
                                 bnbw        INTEGER,
                                 qlimit      INTEGER,
+                                delay       INTEGER,
                                 rrate       INTEGER,
                                 rdelay      INTEGER,
+                                ackreor     INTEGER,
+                                ackloss     INTEGER,
                                 rtos        INTEGER,
                                 frs         INTEGER,
                                 iterationNo INTEGER,
@@ -297,8 +390,11 @@ class ReorderingAnalysis(Analysis):
             return
 
         # Do Plots
-        for y in ("thruput", "frs", "rtos"):
-            self.generateYOverXLinePlot(y)
+        if self.options.fairness:
+            self.generateFairnessOverXLinePlot()
+        else:
+            for y in ("thruput", "frs", "rtos"):
+                self.generateYOverXLinePlot(y)
 
 
     def main(self):
