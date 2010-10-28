4c358db2642cf5d961caa8c5f853f592e7f6dda8
Lennart Schulte <lennart.schulte@rwth-aachen.de>
2010-10-28 10:36:48 +0000

    old version with new styles :)

--- pdfconverters/um_gnuplot2pdf.py
+++ pdfconverters/um_gnuplot2pdf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env /usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Generates pdfs from gnuplot epslatex files.
@@ -39,11 +39,11 @@ class Gnuplot2PDF(Application):
         self._latex = None
 
         # initialization of the option parser
-        usage = "usage: %prog [options] <gplotdir> ...\n"\
+        usage = "usage: %prog [options] <gplot> | <gplotdir> ...\n"\
                 "       where 'gplotdir' is directory which contains one *.gpl or *.gplot file"
 
         self.parser.set_usage(usage)
-        self.parser.set_defaults(force = False, outdir = os.getcwd(), fontsize = 9)
+        self.parser.set_defaults(force = False, outdir = os.getcwd(), fontsize = 6, plotsize="14.4cm,9.7cm")
 
         self.parser.add_option("-f", "--force",
                                action = "store_true", dest = "force",
@@ -63,9 +63,10 @@ class Gnuplot2PDF(Application):
         self.parser.add_option("-z", "--font-size", metavar = "NUM", type = int,
                                action = "store", dest = "fontsize",
                                help = "set font size [default: %default]")
-        self.parser.add_option("-t", "--dont-replot",
-                                dest = "dontreplot", action = "store_true",
-                                help = "dont replot file, use previous eps")
+        self.parser.add_option("-p", "--plot-size", metavar = "NUM,NUM",
+                               action = "store", dest = "plotsize",
+                               help = "set font size [default: %default]")
+
     def set_option(self):
         """Set options"""
 
@@ -83,6 +84,7 @@ class Gnuplot2PDF(Application):
         self._latex.setDocumentclass("scrartcl", "fontsize=%spt" %self.options.fontsize)
         self._latex.addSetting(r"\renewcommand{\familydefault}{\sfdefault}")
         self._latex.addSetting(r"\usepackage{sfmath}")
+
     def run(self):
         """Main method of the Gnuplot2PDF object"""
 
@@ -90,21 +92,21 @@ class Gnuplot2PDF(Application):
         srcdir = os.getcwd()
         destdir = self.options.outdir
         tempdir = tempfile.mkdtemp()   
-        target = "" 
+        target = ""
         # work in tempdir
         if self.options.debug:
             debug("Change directory %s" %tempdir)
         os.chdir(tempdir)
-                
+
 	    # plot all plotfiles and put them into one latex document
         for index, gplot in enumerate(self.args):
-       
+
             # get the full path of the figure or directory
             gplotSrc = os.path.join(srcdir, gplot)
-        
+
             # 'gplotSrc' is a directory
             if os.path.isdir(gplotSrc):
-            
+
                 for entry in os.listdir(gplotSrc):
                     # get the full path
                     fileSrc = os.path.join(gplotSrc, entry)
@@ -114,47 +116,44 @@ class Gnuplot2PDF(Application):
                     ext = os.path.splitext(entry)[1]
                     if ext == ".gpl" or ext == ".gplot":
                         target=os.path.join(entry)
-                
+
             # 'gplotSrc' is neither a directory nor file
             elif not os.path.isfile(gplotSrc):
                 warn("%s is not a regular file or directory. Skipped." %gplot)
                 continue
             else:
                 target=gplotSrc
-               
+
             if not target:
                 exit("no gnuplot files found")
-                
+
             # get the basename (without extension)
             if self.options.basename:
-                basename = "%s-%s" %(self.options.basename, index)
+                basename = "%s_%s" %(self.options.basename, index)
             else:
-                basename = os.path.basename(target)
+                basename = os.path.basename(gplot)
                 basename = os.path.splitext(basename)[0]
-                
+
             # build names for output files
-            gplotEps = "%s.eps" %basename
-            gplotPdf = "%s.pdf" %basename
-            gplotTex = "%s.tex" %basename
+            gplotEps = "%s_gplot.eps" %basename
+            gplotPdf = "%s_gplot.pdf" %basename
+            gplotTex = "%s_gplot.tex" %basename
 
             # build gnuplot object and load the given gnuplot file
             # be safe; print a new line if the load file include a "pause" command
-            info("Run gnuplot on %s..." %gplot)            
+            info("Run gnuplot on %s..." %gplot)
             gp = Gnuplot.Gnuplot(debug = int(self.options.debug))
-            if not self.options.dontreplot:
-                gp('set terminal epslatex color colortext solid "default" %s' %self.options.fontsize)
-                gp('set output "%s"'%gplotTex)
-                gp.load(target)
-                gp("\n")
-            
+            gp.load(target)
+            gp("\n")
+
             # replot with epslatex terminal
-            if not self.options.dontreplot:
-                gp('set terminal epslatex color colortext solid "default" %s' %self.options.fontsize)
-                gp('set output "%s"' %gplotTex)
-                gp("replot")
-                gp.close()
-                        
-            # convert the EPS file to a PDF file   
+            gp('set terminal epslatex input color colortext solid "default" size %s font %u' %(self.options.plotsize,(int(round(self.options.fontsize*1.2)) )) )
+            gp('set output "%s"' %gplotTex)
+            gp("replot")
+            gp.save("bla.gplot")
+            gp.close()
+
+            # convert the EPS file to a PDF file
             info("Run epstopdf on %s..." %gplot)
             if self.options.debug:
                 cmd = "epstopdf --debug --outfile=%s %s" %(gplotPdf, gplotEps)
@@ -167,19 +166,19 @@ class Gnuplot2PDF(Application):
             if self.options.save_figures:
                 gplotPdfDst = os.path.join(destdir, gplotPdf)
                 gplotTexDst = os.path.join(destdir, gplotTex)
-            
+
                 if not self.options.force and os.path.exists(gplotPdfDst):
                     error("%s already exists. Skipped." %gplotPdfDst)
                 else:
                     info("Save gnuplot PDF file to %s..." %gplotPdfDst)
                     shutil.copy(gplotPdf, gplotPdfDst)
-                              
+
                 if not self.options.force and os.path.exists(gplotTexDst):
                     error("%s already exists. Skipped." %gplotTexDst)
                 else:
                     info("Save gnuplot Tex file to %s..." %gplotTexDst)
                     shutil.copy(gplotTex, gplotTexDst)
-                    
+
             # add new generated to the latex doc
             self._latex.addLatexFigure(gplotTex, basename)
 
@@ -187,13 +186,13 @@ class Gnuplot2PDF(Application):
         if self.options.texfile:
            info("Save main LaTeX file...")
            self._latex.save()
-            
+
         # build pdf graphics
         info("Generate PDF files...")
         self._latex.toPdf(tempdir = tempdir)
 
         # clean up
-        shutil.rmtree(tempdir)
+        #shutil.rmtree(tempdir)
 
     def main(self):
         self.parse_option()
