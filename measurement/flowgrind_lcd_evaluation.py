7ebdc8982ed62197e3d5eebe917ff0dd2a8b495e
Christian Samsel <christian.samsel@rwth-aachen.de>
2010-12-14 15:17:11 +0000

    script: update lcd measurement

--- measurement/flowgrind_lcd_evaluation.py
+++ measurement/flowgrind_lcd_evaluation.py
@@ -30,8 +30,8 @@ class TcpMeasurement(measurement.Measurement):
         # common options used for all tests
         opts = dict( flowgrind_cc = "reno",
                      flowgrind_bin = "flowgrind-lcd",
-                     flowgrind_duration = 240,
-                     flowgrind_warmup = 5,
+                     flowgrind_duration = 180,
+                     flowgrind_warmup = 2
                      )
 
         brokenhosts = ["mrouter12", "mrouter22", "mrouter23", "mrouter24", "mrouter28", "mrouter41", "mrouter43"]
@@ -40,10 +40,8 @@ class TcpMeasurement(measurement.Measurement):
         # inner loop configurations
         runs = [
                  dict( run_label = r"src14-dst8", src=14, dst=8 ),
-                 dict( run_label = r"src14-dst27", src=14, dst=27 ),
-                 dict( run_label = r"src14-dst30", src=14, dst=30 ),
-                 dict( run_label = r"src14-dst18", src=14, dst=18 ),
-                 dict( run_label = r"src14-dst2", src=14, dst=2 )
+                 dict( run_label = r"src14-dst17", src=14, dst=17 ),
+                 dict( run_label = r"src14-dst6", src=14, dst=6 ),
                ]
 
         # repeat loop
@@ -52,17 +50,16 @@ class TcpMeasurement(measurement.Measurement):
         # outer loop with different scenario settings
         scenarios   = [
         dict( scenario_label = "bulk",
-            flowgrind_opts ="-n 2 -M s -F 1 -O b=TCP_LCD".split()),
-        #dict( scenario_label = "rr-http",
-        #    flowgrind_opts="-r 123456 -n 2 -G s=q,C,350 -G s=p,L,9055,115.17 -U 100000 -F1 -O b=TCP_LCD".split() ),
-        #dict( scenario_label = "rr-smtp",
-        #    flowgrind_opts="-r 654321 -n 2 -G s=q,U,500,20000 -G s=p,C,80 -F1 -O b=TCP_LCD".split() ),
-        #dict( scenario_label = "rr-telnet",
-        #    flowgrind_opts="-r 123654 -n 2 -G s=q,U,40,1000 -G s=q,U,40,1000 -F1 -O b=TCP_LCD,TCP_NODELAY -F2 b=TCP_NODELAY".split() ),
-        #dict( scenario_label = "limited-normal-800kbs",
-        #    flowgrind_opts="-r 987654 -n 2 -G s=g,N,0.08192,0.1 -F1 -O b=TCP_LCD,TCP_NODELAY -F2 b=TCP_NODELAY".split() ),
+              flowgrind_opts ="-n 2 -M s -F0 -O b=TCP_LCD".split()),
+        dict( scenario_label = "rr-http",
+              flowgrind_opts="-r 123456 -M s -n 2 -G s=q,C,350 -G s=p,L,9055,115.17 -U 100000 -F0 -O b=TCP_LCD".split() ),
+        dict( scenario_label = "rr-smtp",
+              flowgrind_opts="-r 654321 -M s -n 2 -G s=q,U,5000,40000 -G s=p,C,120 -F0 -O b=TCP_LCD".split() ),
+        dict( scenario_label = "rr-telnet",
+              flowgrind_opts="-r 123654 -M s -n 2 -G s=q,U,40,10000 -G s=q,U,40,10000 -F0 -O b=TCP_LCD,b=TCP_NODELAY -F1 -O b=TCP_NODELAY".split() ),
+        dict( scenario_label = "streaming-media-limited-800kbs",
+              flowgrind_opts="-r 987654 -M s -n 2 -G s=q,C,800 -G s=g,N,0.008,0.001 -F0 -O b=TCP_LCD,b=TCP_NODELAY -F1 -O b=TCP_NODELAY".split() ),
         ]
-
         # configure testbed
 
         yield self.switchTestbedProfile("flowgrind_lcd_evaluation")
@@ -81,6 +78,13 @@ class TcpMeasurement(measurement.Measurement):
         yield self.remote_execute_many(srchosts, "sudo ip route add 169.254.9.0/24 via 169.254.9.15")
 
         yield self.remote_execute_many(middlehosts, "sudo ip route add 169.254.9.14 dev ath0")
+
+        # activate icmp logging
+        yield self.remote_execute_many(srchosts, "sudo iptables -I INPUT -i ath0 -p icmp --icmp-type 3/0 -j LOG")
+        yield self.remote_execute_many(srchosts, "sudo iptables -I INPUT -i ath0 -p icmp --icmp-type 3/1 -j LOG")
+        yield self.remote_execute_many(allhosts, "sudo iptables -I OUTPUT -i ath0 -p icmp --icmp-type 3/0 -j LOG")
+        yield self.remote_execute_many(allhosts, "sudo iptables -I OUTPUT -i ath0 -p icmp --icmp-type 3/1 -j LOG")
+
         # wait a few minutes to let olsr converge
         yield twisted_sleep(2)
 
@@ -95,8 +99,7 @@ class TcpMeasurement(measurement.Measurement):
                     target_dir = "%s/%s/%s" %(self.options.log_dir,scenario['scenario_label'],run['run_label'])
                     if not os.path.exists(target_dir):
                             os.makedirs(target_dir)
-                    self.logprefix="%s/%s/i%03u" %(scenario['scenario_label'],run['run_label'], it)
-
+                    self.logprefix="%s/%s/i%03u_s%u_r%u" %(scenario['scenario_label'],run['run_label'], it, scenario_no, run_no)
 
                     # set source and dest for tests
                     kwargs['flowgrind_src'] = kwargs['src']
@@ -106,7 +109,7 @@ class TcpMeasurement(measurement.Measurement):
                     yield self.run_test(tests.test_flowgrind, **kwargs)
 
         # return to status quo
-        yield self.switchTestbedProfile("minimum")
+        yield self.switchTestbedProfile("minimum2010")
 
         yield self.tear_down()
         reactor.stop()
