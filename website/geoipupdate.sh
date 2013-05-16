dace36e8bda61a013adb28e914f1a0eb16ebf605
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-05-16 16:18:12 +0200

    Delete stuff which is uninteresting for NetApp

--- website/geoipupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-URL="http://www.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
-DST=/usr/local/share/GeoIP/
-
-cd $DST
-/usr/bin/wget -nd -r -N --progress=dot $URL
-/bin/gunzip -f $DST/GeoIP.dat.gz --to-stdout > $DST/GeoIP.dat
