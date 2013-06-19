3793d242628f12973e45ef41ab0dcb9edb95c6b8
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-06-19 13:39:20 +0200

    Split the scripts folder into 3 new repos   - tcp-analysis   - administration   - python-libs Here, I delete now all admin stuff and the libs.

--- backup/mysql-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-umask 027
-BACKUP_DIR=${1:-/dumps/mysql}
-BACKUP_FILE="mysql_dumpall"
-
-if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
-/usr/bin/mysqldump --all-databases | gzip --rsyncable > $BACKUP_DIR/$BACKUP_FILE.gz
--- backup/openldap-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-umask 027
-BACKUP_DIR=${1:-/backup/openldap}
-BACKUP_FILE="ldap_dump.ldif"
-
-if [ ! -d $BACKUP_DIR ]; then mkdir -p $BACKUP_DIR; fi
-slapcat | gzip > $BACKUP_DIR/$BACKUP_FILE.gz
