1cb233af97e6490b08228f49bb83f15bb3d9d332
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-07-05 10:40:52 +0100

    Application object uses now argparse instead of optparse. Every script that use the application object must be rewritten

--- application.py
+++ application.py
@@ -16,76 +16,64 @@
 # python imports
 import logging
 import logging.handlers
-import optparse
+import argparse
 
 class Application(object):
-    """Framework for UMIC-Mesh applications"""
+    """Framework for generic Applications"""
 
-    def __init__(self):
-        """Constructor of the generic Application object"""
+    def __init__(self, prog=None, usage=None, description=None, epilog=None):
+        """Constructor for generic Application object"""
 
         # object variables
-        self.parser = optparse.OptionParser()
+        self.parser = argparse.ArgumentParser(prog=prog, usage=usage,
+                description=description, epilog=epilog)
+        self._log_group = self.parser.add_mutually_exclusive_group()
 
-        # initialization of the option parser
-        usage = "usage: %prog [options]"
-        self.parser.set_usage(usage)
-        self.parser.set_defaults(syslog = False, server = "logserver",
-                                 verbose = True, debug = False)
-        self.parser.add_option("--syslog",
-                action = "store_true", dest = "syslog",
-                help = "log to syslog")
-        self.parser.add_option("--syslog-server", type="string", metavar = "HOST",
-                action = "callback", callback=self.check_option, dest = "server",
-                help = "sends log to a specific server [default: %default]")
-        self.parser.add_option("-v", "--verbose",
-                action = "store_true", dest = "verbose",
-                help = "being more verbose [default]")
-        self.parser.add_option("-q", "--quiet",
-                action = "store_false", dest = "verbose",
-                help = "being more quiet")
-        self.parser.add_option("--debug",
-                action = "store_true", dest = "debug",
-                help = "being even more verbose")
+        # initialization of the argument parser
+        self._log_group.add_argument("-q", "--quiet", action = "store_false",
+                default=False, help="being more quiet")
+        self._log_group.add_argument("-v", "--verbose", action="store_true",
+                default=False, help="being more verbose")
+        self._log_group.add_argument("--debug", action="store_true",
+                default=False, help="being even more verbose")
+        self.parser.add_argument("--syslog", metavar="HOST", action="store",
+                nargs="?", const="localhost", help="log to syslog server "\
+                        "%(metavar)s. (default: %(const)s)")
 
-    def check_option(self, option, opt_str, value, parser):
-        """Check the options for generic Application object"""
-        # if this option is set, the "--syslog" option is also enabled
-        if opt_str == "--syslog-server":
-            parser.values.syslog = True
+    def parse_options(self, args = None):
+        """Parse options for generic Application object"""
 
-    def parse_option(self, args = None):
-        """Parse the options for generic Application object"""
         # parse options
-        (self.options, self.args) = self.parser.parse_args(args)
+        self.args = self.parser.parse_args(args)
 
-    def set_option(self):
-        """Set the options for generic Application object"""
+    def apply_options(self):
+        """Configure generic Application object based on the options from
+        the argparser"""
 
-        # being verbose?
-        if self.options.debug:
+        # setting log level
+        if self.args.debug:
             log_level = logging.DEBUG
-        elif self.options.verbose:
+        elif self.args.verbose:
             log_level = logging.INFO
+        elif self.args.quiet:
+            log_level = logging.ERROR
         else:
             log_level = logging.WARNING
 
-        # using syslog?
-        if self.options.syslog:
-            syslog_host = (self.options.server, 514)
+        # using syslog
+        if self.args.syslog:
+            syslog_host = (self.args.syslog, 514)
             syslog_facility = logging.handlers.SysLogHandler.LOG_DAEMON
-            syslog_format = self.parser.get_prog_name() + \
-                            "[%(levelname)s]: %(message)s"
-            syslog_Handler = logging.handlers.SysLogHandler(
-                                syslog_host, syslog_facility)
+            syslog_format = "%(prog)s [%(levelname)s]: %(message)s"
+            syslog_Handler = logging.handlers.SysLogHandler(syslog_host,
+                    syslog_facility)
             syslog_Handler.setFormatter(logging.Formatter(syslog_format))
             logging.getLogger("").addHandler(syslog_Handler)
             logging.getLogger("").setLevel(log_level)
-
         # using standard logger
         else:
             log_format = "%(asctime)s %(levelname)s: %(message)s"
             log_datefmt = "%b %d %H:%M:%S"
-            logging.basicConfig(level = log_level, format = log_format,
-                                datefmt = log_datefmt)
+            logging.basicConfig(level=log_level, format=log_format,
+                    datefmt=log_datefmt)
 
