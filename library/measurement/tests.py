80814498d1d6063ce7accda84b62dcd8cb5c4ff8
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-10-07 18:04:10 +0200

    library/measurement/tests.py has no dependency to um_node anymore

--- library/measurement/tests.py
+++ library/measurement/tests.py
@@ -27,129 +27,198 @@ from network.functions import twisted_call, twisted_execute
 """This module should collect standard test methods."""
 
 @defer.inlineCallbacks
-def test_ping(mrs,
-              log_file,
-              ping_src,
-              ping_dst,
-              ping_size     = 56,
-              ping_interval = 1,
-              ping_count    = 10,
-              ping_opts     = "",
-              ping_iface    = "ath0",
-              **kwargs ):
+def test_ping(mrs, log_file, src, dst, size=56, interval=1, count=10, opts="",
+        **kwargs):
     """This test performs a simple ping from src to dst.
 
        required arguments:
-           mrs        : reference to parent measurement class
-           log_file   : file descriptor where the results are written to
-           ping_src   : sender of the pings
-           ping_dst   : receiver of the pings
+           mrs     : reference to parent measurement class
+           log_file: file descriptor where the results are written to
+           src     : sender of the pings
+           dst     : receiver of the pings
 
        optional arguments:
-           ping_size    : size in bytes of the packets send out
-           ping_count   : how many packets should be send out
-           ping_interval: time between to pings in seconds
-           ping_opts    : additional ping options
+           size    : size in bytes of the packets send out
+           count   : how many packets should be send out
+           interval: time between to pings in seconds
+           opts    : additional ping options
     """
 
-    # for convenience accept numbers as src and dst
-    src = Node(ping_src, node_type="meshrouter")
-    dst = Node(ping_dst, node_type="meshrouter")
-
-    dst_ip = yield mrs.getIp(dst.getHostname(), ping_iface)
-
-    cmd = "ping -i %.3f -c %u -s %u %s %s" % (ping_interval, ping_count,
-                                              ping_size, ping_opts,
-                                              dst_ip)
-    rc = yield mrs.remote_execute(src.getHostname(),
-                                  cmd,
-                                  log_file,
-                                  timeout=int((ping_interval*ping_count)+5))
+    cmd = "ping -i %.3f -c %u -s %u %s %s" % (interval, count, size, opts, ip)
+    rc = yield mrs.remote_execute(src, cmd, log_file,
+                                  timeout=int((interval * count) + 5))
     defer.returnValue(rc)
 
 @defer.inlineCallbacks
-def test_fping(mrs,
-              log_file,
-              ping_src,
-              ping_dst,
-              ping_size     = 56,
-              ping_interval = 1,
-              ping_count    = 10,
-              ping_iface    = "ath0",
-              fping_opts    = "",
-              **kwargs ):
+def test_fping(mrs, log_file, src, dst, size=56, interval=1, count=10, opts="",
+        **kwargs):
     """This test performs a simple ping from src to dst. Drop in replacement for ping.
 
        required arguments:
-           mrs        : reference to parent measurement class
-           log_file   : file descriptor where the results are written to
-           ping_src   : sender of the pings
-           ping_dst   : receiver of the pings
+           mrs     : reference to parent measurement class
+           log_file: file descriptor where the results are written to
+           src     : sender of the pings
+           dst    : receiver of the pings
 
        optional arguments:
-           ping_size    : size in bytes of the packets send out
-           ping_count   : how many packets should be send out
-           ping_interval: time between to pings in seconds
-           fping_opts   : additional fping options
+           size    : size in bytes of the packets send out
+           count   : how many packets should be send out
+           interval: time between to pings in seconds
+           opts    : additional fping options
     """
 
-    # for convenience accept numbers as src and dst
-    src = Node(ping_src, node_type="meshrouter")
-    dst = Node(ping_dst, node_type="meshrouter")
-
-    dst_ip = yield mrs.getIp(dst.getHostname(), ping_iface)
-
-    cmd = "fping -A -p %u -c %u -b %u %s %s 2>&1" % ((ping_interval*1000), ping_count,
-                                             ping_size, fping_opts,
-                                             dst_ip)
+    cmd = "fping -A -p %u -c %u -b %u %s %s 2>&1" % ((interval * 1000), count,
+            size, opts, dst)
 
     # calculate conservative timeout with extra 10% and 5 seconds safeguard
-    time_out = int(ping_interval*ping_count*1.1+5)
-    rc = yield mrs.remote_execute(src.getHostname(),
-                                  cmd,
-                                  log_file,
-                                  timeout=time_out)
+    time_out = int(interval * count * 1.1 + 5)
+
+    rc = yield mrs.remote_execute(src, cmd, log_file, timeout=time_out)
     defer.returnValue(rc)
 
 @defer.inlineCallbacks
-def test_thrulay(mrs,
-                 log_file,
-                 thrulay_src,
-                 thrulay_dst,
-                 thrulay_duration = 15,
-                 thrulay_cc     = "reno",
-                 thrulay_opts   = "",
-                 **kwargs ):
+def test_thrulay(mrs, log_file, src, dst, dst_ctrl=None, duration=15,
+        cc="reno", opts="", **kwargs):
     """This test performs a simple thrulay test with one tcp
        flow from src to dst.
 
        required arguments:
-            log_file   : file descriptor where the results are written to
-            mrs        : reference to parent measurement class
-            thrulay_src: sender of the flow
-            thrulay_dst: receiver of the flow
+            mrs     : reference to parent measurement class
+            log_file: file descriptor where the results are written to
+            src     : sender of the flow
+            dst     : receiver of the flow
 
        optional arguments:
-            thrulay_duration: duration of the flow in seconds
-            thrulay_cc      : congestion control method to use
-            thrulay_opts    : additional command line arguments
+            dst_ctrl: control connection to the receiver
+            duration: duration of the flow in seconds
+            cc      : congestion control method to use
+            opts    : additional command line arguments
     """
 
-    # for convenience accept numbers as src and dst
-    src = Node(thrulay_src, node_type="meshrouter")
-    dst = Node(thrulay_dst, node_type="meshrouter")
-
-    dst_ip = yield mrs.getIp(dst.getHostname(), "ath0")
-
-    cmd = "thrulay -Q -c %s -t %.3f -H %s/%s" % (thrulay_cc,
-                                               thrulay_duration,
-                                               dst_ip,
-                                               dst.getHostname())
-    rc = mrs.remote_execute(src.getHostname(),
-                            cmd,
-                            log_file,
-                            timeout=thrulay_duration+5)
+    if dst_ctrl:
+        cmd = "thrulay -Q -c %s -t %.3f -H %s/%s" % (cc, duration, dst,
+                dst_ctrl)
+    else:
+        cmd = "thrulay -Q -c %s -t %.3f -H %s" % (cc, duration, dst)
+    rc = mrs.remote_execute(src, cmd, log_file, timeout=duration + 5)
     defer.returnValue(rc)
+
+@defer.inlineCallbacks
+def test_flowgrind(mrs, log_file, src, dst, src_ctrl=None, dst_ctrl=None,
+                   timeout=10, duration=15, warmup=0, cc=None, dump=None,
+                   bport=5999, opts=[], fg_bin="flowgrind", gzip_dumps=True,
+                   **kwargs ):
+    """This test performs a simple flowgrind (new, aka dd version) test with
+    one tcp flow from src to dst.
+
+       required arguments:
+            mrs     : reference to parent measurement class
+            log_file: file descriptor where the results are written to
+            src     : sender of the flow
+            dst     : receiver of the flow
+
+       optional arguments:
+            src_ctrl : control connection to the sender
+            dst_ctrl : control connection to the receiver
+            duration : duration of the flow in seconds
+            cc       : congestion control method to use
+            warmup   : warmup time for flow in seconds
+            dump     : turn tcpdump on src and dst on iface 'dump' on
+            bport    : flowgrind base port
+            opts     : additional command line arguments
+            fg_bin   : flowgrind binary
+            gzip_pcap: gzip dumps to save space
+    """
+
+    # path of executable
+    cmd = [ fg_bin ]
+
+    # add -p for numerical output
+    cmd.extend(["-p"])
+
+    # test duration
+    cmd.extend(["-T", "s=%.2f" % duration])
+
+    # inital delay
+    if warmup:
+        cmd.extend(["-Y", "s=%.2f" % warmup])
+
+    # which tcp congestion control module
+    if cc:
+        cmd.extend(["-O", "s=TCP_CONG_MODULE=%s" % cc])
+
+    # control connections in place?
+    if not src_ctrl:
+        src_ctrl = src
+    if not dst_ctrl:
+        dst_ctrl = dst
+
+    # build host specifiers
+    cmd.extend(["-H", "s=%s/%s,d=%s/%s" %(src, src_ctrl, dst, dst_ctrl)])
+
+    # just add additional parameters
+    if opts:
+        cmd.extend(opts)
+
+    # start tcpdump
+    if dump:
+        raise NotImplementedError, "bport must be re-added to new flowgrind first"
+
+        dumpfile_src = None
+        dumpfile_dst = None
+        results = yield mrs.xmlrpc_many([src_ctrl, dst_ctrl], "tcpdump.start",
+            dump, "port %u" %bport )
+        debug(results)
+
+        # shortcuts
+        sres = results[0]
+        dres = results[1]
+        if sres[0]:
+            dumpfile_src = sres[1]
+        else:
+            warn("Failed to start tcpdump on %s: %s" %(src_ctrl,
+                sres[1].getErrorMessage()))
+        if dres[0]:
+            dumpfile_dst = dres[1]
+        else:
+            warn("Failed to start tcpdump on %s: %s" %(dst_ctrl,
+                dres[1].getErrorMessage()))
+
+    # run flowgrind
+    result = yield mrs.local_execute(" ".join(cmd), log_file, timeout=(duration
+        + warmup + timeout))
+
+    # stop tcpdump
+    if dump:
+        yield mrs.xmlrpc_many([src_ctrl, dst_ctrl], "tcpdump.stop")
+
+        # just schedule moving and compressing for later execution
+        if dumpfile_src:
+            # just append .hostname.pcap to logfilename
+            sfile = "%s.%s.pcap" %(log_file.name, src_ctrl)
+            cmd = ["mv", dumpfile_src, sfile]
+            d = twisted_call(cmd, shell=False)
+            if gzip_pcap:
+                def callback(rc):
+                    if rc != 0:
+                        return
+                    cmd = ["gzip", sfile]
+                    twisted_call(cmd, shell=False)
+
+                d.addCallback(callback)
+        if dumpfile_dst:
+            # just append .hostname.pcap to logfilename
+            dfile = "%s.%s.pcap" %(log_file.name, dst_ctrl)
+            cmd = ["mv", dumpfile_dst, dfile]
+            d=twisted_call(cmd, shell=False)
+            if gzip_dumps:
+                def callback(rc):
+                    if rc != 0:
+                        return
+                    cmd = ["gzip", dfile]
+                    twisted_call(cmd, shell=False)
+
+                d.addCallback(callback)
 
 @defer.inlineCallbacks
 def test_multiflowgrind(mrs,
@@ -241,137 +310,6 @@ def test_multiflowgrind(mrs,
 
     result = yield mrs.local_execute(" ".join(cmd), log_file,
             timeout=(max_duration + flowgrind_timeout) )
-
-    defer.returnValue(result)
-
-@defer.inlineCallbacks
-def test_flowgrind(mrs,
-                   log_file,
-                   flowgrind_src,
-                   flowgrind_dst,
-                   flowgrind_timeout = 10,
-                   flowgrind_duration = 15,
-                   flowgrind_warmup = 0,
-                   flowgrind_cc     = None,
-                   flowgrind_dump   = False,
-                   flowgrind_iface  = "ath0",
-                   flowgrind_bport  = 5999,
-                   flowgrind_bin    = "flowgrind",
-                   flowgrind_opts   = [],
-                   gzip_dumps       = True,
-                   nodetype         = "meshrouter",
-                   **kwargs ):
-    """This test performs a simple flowgrind (new, aka dd version) test with one tcp
-       flow from src to dst.
-
-       required arguments:
-            log_file      : file descriptor where the results are written to
-            mrs           : reference to parent measurement class
-            flowgrind_src : sender of the flow
-            flowgrind_dst : receiver of the flow
-
-       optional arguments:
-            flowgrind_duration : duration of the flow in seconds
-            flowgrind_warmup   : warmup time for flow in seconds
-            flowgrind_cc       : congestion control method to use
-            flowgrind_dump     : turn on tcpdump on src and sender
-            flowgrind_iface    : iface to get ipaddress
-            flowgrind_bport    : flowgrind base port
-            flowgrind_bin      : flowgrind binary
-            flowgrind_opts     : additional command line arguments
-            flowgrind_timeout  : additional time to wait after duration before stopping the flow
-            gzip_dumps         : gzip dumps to save space
-            nodetype           : the nodetype ("e.g. vmeshrouter")
-    """
-
-    # for convenience accept numbers as src and dst
-    src = Node(flowgrind_src, node_type=nodetype)
-    dst = Node(flowgrind_dst, node_type=nodetype)
-
-    # ips of the measurement interfaces
-    src_ip = yield mrs.getIp(src.getHostname(), flowgrind_iface)
-    dst_ip = yield mrs.getIp(dst.getHostname(), flowgrind_iface)
-
-    # path of executable
-    cmd = [ flowgrind_bin ]
-
-    # add p for numerical output
-    cmd.extend(["-p"])
-
-    # options
-    cmd.extend(["-T", "s=%.2f" % flowgrind_duration])
-
-    if flowgrind_warmup:
-        cmd.extend(["-Y", "s=%.2f" % flowgrind_warmup])
-
-    if flowgrind_cc:
-        cmd.extend(["-O", "s=TCP_CONG_MODULE=%s" % flowgrind_cc])
-
-    # build host specifiers
-    cmd.extend(["-H", "s=%s/%s,d=%s/%s" % (src_ip, src.getHostname(),
-                                           dst_ip, dst.getHostname()) ])
-
-    # just add additional parameters
-    if flowgrind_opts:
-        cmd.extend(flowgrind_opts)
-
-    if flowgrind_dump:
-        raise NotImplementedError, "bport must be re-added to new flowgrind first"
-
-        dumpfile_src = None
-        dumpfile_dst = None
-        results = yield mrs.xmlrpc_many([src.getHostname(),dst.getHostname()],
-                                       "tcpdump.start",
-                                       flowgrind_iface,
-                                       "port %u" %flowgrind_bport )
-        debug(results)
-        # shortcuts
-        sres = results[0]
-        dres = results[1]
-        if sres[0]:
-            dumpfile_src = sres[1]
-        else:
-            warn("Failed to start tcpdump on %s: %s" %(src.getHostname(),
-                                                       sres[1].getErrorMessage()))
-        if dres[0]:
-            dumpfile_dst = dres[1]
-        else:
-            warn("Failed to start tcpdump on %s: %s" %(dst.getHostname(),
-                                                       dres[1].getErrorMessage()))
-
-    result = yield mrs.local_execute(" ".join(cmd), log_file,
-            timeout=(flowgrind_duration + flowgrind_warmup + flowgrind_timeout) )
-
-    if flowgrind_dump:
-        yield mrs.xmlrpc_many([src.getHostname(),dst.getHostname()],
-                              "tcpdump.stop")
-        # just schedule moving and compressing for later execution
-        if dumpfile_src:
-            # just append .hostname.pcap to logfilename
-            sfile = "%s.%s.pcap" %(log_file.name, src.getHostname())
-            cmd = ["mv",dumpfile_src, sfile]
-            d=twisted_call(cmd, shell=False)
-            if gzip_dumps:
-                def callback(rc):
-                    if rc != 0:
-                        return
-                    cmd = ["gzip", sfile]
-                    twisted_call(cmd, shell=False)
-
-                d.addCallback(callback)
-        if dumpfile_dst:
-            # just append .hostname.pcap to logfilename
-            dfile = "%s.%s.pcap" %(log_file.name, dst.getHostname())
-            cmd = ["mv",dumpfile_dst, dfile]
-            d=twisted_call(cmd, shell=False)
-            if gzip_dumps:
-                def callback(rc):
-                    if rc != 0:
-                        return
-                    cmd = ["gzip", dfile]
-                    twisted_call(cmd, shell=False)
-
-                d.addCallback(callback)
 
     defer.returnValue(result)
 
